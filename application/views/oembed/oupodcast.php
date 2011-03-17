d080ea55cbdd3f9dff5caf205e344ae5db9b5ab9
Nick Freear <nfreear@yahoo.co.uk>
2011-03-17 15:12:31 +0000

    Initial oEmbed view for oupodcast - jwPlayer in <object> to match http://podcast.open.ac.uk

--- application/views/oembed/oupodcast.php
+++ application/views/oembed/oupodcast.php
@@ -1,21 +1,32 @@
 <?php
 /** oEmbed OU podcast view.
 
-TODO. ou podcast.
+NDF, 17 March 2011.
 */
-  $width = 550;
-  $height= 400;
+  $width = 608;
+  $height= 362;
+  $pod_base = 'http://podcast.open.ac.uk';
 
+// Need to fork for video and audio.
+
+
+// Get something rolling, using the existing jwPlayer!
+
+
+//(file=http://podcast.open.ac.uk/feeds/l314-spanish/rss2.xml&javascriptid=flashplayer&enablejs=true)
   $html =<<<EOF
-<div class="ou podcast oembed"><object type="application/x-shockwave-flash" width="$width" height="$height" data="http://prezi.com/bin/preziloader.swf">
-<param name="movie" value="--"/>
-<param name="allowfullscreen" value="true"/>
-<param name="allowscriptaccess" value="always"/>
-<param name="bgcolor" value="#ffffff"/>
-<param name="flashvars" value="---" />
-<p>Your browser needs Flash enabled to view this presentation.</p>
-<img alt="" src="$meta->thumbnail_url"/></object><div><img alt="" src="http://www.open.ac.uk/favicon.ico" />
-<a href="$meta->url">$meta->title</a>  on <a href="http://podcast.open.ac.uk/">OU Podcast</a>.</div>
+<div class="ou podcast oembed">
+<object tabindex="0" aria-label="Media player" type="application/x-shockwave-flash" height="$height" width="$width"
+data="$pod_base/flash_media_player/mediaplayer.swf" >
+<param name="movie" value="data="$pod_base/flash_media_player/mediaplayer.swf" />
+<param name="allowscriptaccess" value="always" />
+<param name="allowfullscreen" value="true" />
+<param name="flashvars" value=
+"displaywidth=$width&amp;width=$width&amp;height=$height&amp;linkfromdisplay=false&amp;__showdownload=false&amp;overstretch=false&amp;image=$meta->_poster_url&amp;file=$meta->_media_url&amp;backcolor=0x000000&amp;frontcolor=0xFFFFFF&amp;lightcolor=0xdbedff&amp;screencolor=0x000000&amp;autostart=false" />
+<p>Your browser needs Flash enabled to view this $meta->type.</p>
+<img alt="" src="$meta->_poster_url"/>
+</object><div><img alt="" src="http://www.open.ac.uk/favicon.ico" />
+<a href="$meta->url">$meta->title</a> on <a href="http://podcast.open.ac.uk/">OU Podcast</a>.</div>
 </div>
 EOF;
 
@@ -23,20 +34,17 @@ EOF;
         'version'=> '1.0',
         'type'   => $meta->type,
         'provider_name'=> 'OU Podcast',
-        'provider_url' => 'http://podcast.open.ac.uk/',
+        'provider_url' => $pod_base,
         'title'  => $meta->title,
-        //'author_name'=>$meta->author, //'author_url' =>null,
+        //'author_name'=>$meta->author, 'author_url' =>null,
         'width'  => $width,
         'height' => $height,
         'html'   => $html, #'embed_type'=> 'application/x-shockwave-flash',
-        'thumbnail_url'=> $meta->thumbnail_url,
+        'thumbnail_url'=> $meta->_poster_url, #thumbnail or poster.
         #'__meta' => $meta,
+        'copyright'=>$meta->_copyright,
         //'license_url'  => null,
   );
-
-#$meta->cache_created.
-#$tsstring = gmdate('D, d M Y H:i:s ', $meta->timestamp) . 'GMT';
-#header("Last-Modified: $tsstring");
 
   $view_data = array(
       'url'   => $url,
