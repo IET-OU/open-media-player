11fb2778fb699ae98c849895e6e9d2bdd8c51d23
Nick Freear <nfreear@yahoo.co.uk>
2011-03-22 16:46:37 +0000

    OU player/ embed demo - fixing Javascript path, etc.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -10,21 +10,22 @@
 <p><a class=embed
 href="http://podcast.open.ac.uk/oulearn/languages/spanish/podcast-l314-spanish#!fe481a4d1d">Introduction: A Buerno Puerto, on OU podcast</a>
 
-<p><a class=__embed href="http://podcast.open.ac.uk/pod/vc-message-to-staff#!746ee92293">VC message 01-02-2011, on OU podcast*</a>
+<!--<p><a class=embed href="http://podcast.open.ac.uk/pod/vc-message-to-staff#!746ee92293">VC message 01-02-2011, on OU podcast*</a>
+-->
 
 <p><a class=embed href="http://youtube.com/watch?v=NaBBk-kpmL4">Interview with Martin Bean, on YouTube (captions)</a>
 
 <p><a class=embed href="http://lamscommunity.org/lamscentral/sequence?seq_id=1007900">Crime fighting, on LAMS</a>
 
-<p><a class=embed href="http://spreadsheets.google.com/viewform?formkey=dDhQOXpJYkl0VzFEQnZnTkhGcF9DSFE6MQ#height=1150">CLA form, on Google Docs</a>
+<p><a class=embed href="http://spreadsheets.google.com/viewform?formkey=dDhQOXpJYkl0VzFEQnZnTkhGcF9DSFE6MQ#height=1160">CLA form, on Google Docs</a>
 
-<p><a class=__embed href="http://prezi.com/zoidjousoeat/technology-for-the-classroom/">Technology, on Prezi</a>
+<p><a class=embed href="http://prezi.com/zoidjousoeat/technology-for-the-classroom/">Technology, on Prezi</a>
 
 
 <script src=
 "http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <!--<script src="scripts/jquery.oembed.js"></script>-->
-<script src="scripts/jquery_oembed"></script>
+<script src="<?=site_url('scripts/jquery_oembed') ?>"></script>
 <script type="text/javascript">
   $(document).ready(function() {
     $("a.embed"      ).oembed(); //null, { embedMethod: "auto" });
