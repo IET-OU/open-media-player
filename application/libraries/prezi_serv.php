2e6f4dbd96d19cfd9e1ee500aaba24b74156cd2a
Nick Freear <nfreear@yahoo.co.uk>
2011-03-23 22:24:19 +0000

    Re-write Prezi_serv::call method to use Embed.ly - new HTML5 breaks my screen-scraper.

--- application/libraries/prezi_serv.php
+++ application/libraries/prezi_serv.php
@@ -3,13 +3,51 @@
  *
  * @copyright Copyright 2011 The Open University.
  */
-//1 March 2011.
+//1, 23 March 2011.
 
 require_once 'base_service.php';
 
 class Prezi_serv extends Base_service {
 
+  /** Call the Embed.ly service.
+  */
   public function call($url, $matches) {
+
+    $meta = array(
+      'url'=>$url,
+      'provider_name'=>'prezi',
+      'provider_mid' =>$matches[1],
+      'title' => ucfirst(str_replace('-', ' ', $matches[2])),
+      'timestamp'=>null,
+    );
+
+    $json_url = "http://api.embed.ly/1/oembed?format=json&url=$url";
+    $result = $this->_http_request_json($json_url, $spoof=TRUE);
+    if (! $result->success) {
+      die("Error, Prezi_serv woops");
+      return FALSE; //Error.
+    }
+
+    if (preg_match('/^(.*) presented by (.*)$/', $result->json->description, $m_desc)) {
+      $meta['timestamp'] = strtotime($m_desc[1]);
+      $meta['author'] = $m_desc[2];
+    }
+    if (preg_match('/^(.*) by/', $result->json->title, $m_title)) {
+      $meta['title'] = $m_title[1];
+    }
+
+    $meta['thumbnail_url']  = $result->json->thumbnail_url;
+    $meta['thumbnail_width']= $result->json->thumbnail_width;
+    $meta['thumbnail_height']=$result->json->thumbnail_height;
+
+    //$cache_id = $this->embed_cache_model->insert_embed($meta);
+
+    return (object) $meta;
+  }
+
+  /** Pre-Embed.ly screen scraper - broken!
+  */
+  protected function _call_screen_scrape($url, $matches) {
   //protected function _meta_prezi($url, $matches=null) {
 
     $meta = array(
