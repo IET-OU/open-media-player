423dd831436c0f5d19bf88dfde962a5d55c202c3
Nick Freear <nfreear@yahoo.co.uk>
2011-03-24 13:35:22 +0000

    Adding intro. text to OU & VLE demo pages.

--- application/views/vle_demo.php
+++ application/views/vle_demo.php
@@ -4,17 +4,25 @@
 <!DOCTYPE html><html lang="en"><meta charset="utf-8" /><title>OUVLE player demo</title>
 <style>body{font:1em sans-serif;} iframe{display:block; border:1px solid #bbb; border-radius:4px;}</style>
 
-<p><a href="http://learn.open.ac.uk/site/sc">Visit 'Learn' first, to prevent access errors</a></p>
+<h1>OUVLE player</h1> <h2>Prototypes</h2>
 
-<!-- Note you seemingly have to add 5px to the iframe height! -->
+<p>NOTE:  not yet accessible!</p>
+<p>IMPORTANT: <a href="http://learn.open.ac.uk/site/sc">Visit 'Learn' first, to prevent access errors</a>.</p>
+
+
+<!--
+ * Hmm, you seemingly have to add 5px to the iframe height!
+ * Is overflow:hidden risky for accessibility?
+ * caption_url/ image_url: Can be absolute or relative to media_url.
+-->
 <p class="video">
-<iframe tabindex="0" title="Player: Intro" width="512" height="323" frameborder="0" src=
+<iframe tabindex="0" title="Player: Intro" width="512" height="318" frameborder="0" style="overflow:hidden;" src=
 "<?=$embed_url;
 ?>?title=Introduction+to+Fairmead&amp;media_url=http%3A//learn.open.ac.uk/file.php/5195/!via/oucontent/course/100705/k315-0-video1.mp4&caption_url=k315-0-video1.srt&width=512&height=318"
 ></iframe></p>
 
 <p class="audio">
-<iframe tabindex="0" title="Player: Music" width="400" height="65" frameborder="0" src=
+<iframe tabindex="0" title="Player: Music" width="400" height="60" frameborder="0" style="overflow:hidden;" src=
 "<?=$embed_url;
 ?>?title=Music+&copy;Rehab&amp;media_url=http%3A//learn.open.ac.uk/file.php/5195/!via/oucontent/course/137628/20070330_rehab-after.mp3&amp;width=400&amp;height=60"
 ></iframe></p><!--Was:60 / 65px-->
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,25 +2,46 @@
 
 <meta name="ROBOTS" content="noindex,nofollow" />
 
-<style>body{font:1em sans-serif;} .oembed{margin:1em 0; border:1px solid #ccc; border-radius:5px;} </style>
+<style>body{font:1em sans-serif;} abbr{cursor:pointer;} .oembed{margin:1em 0; border:1px solid #ccc; border-radius:5px;} </style>
 
-<h1>OU player</h1> <h2>Demonstrations</h2>
+<h1>OU player</h1> <h2>Prototype demonstrations</h2>
+
+<p>These are early prototypes for the <abbr title="The Open University">OU</abbr> player/<abbr title=
+"Open University Learning Design Initiative, including Cloudworks">OULDI</abbr> embed projects. Here's an <a href=
+"http://freear.org.uk/content/ou-media-player-project">introductory blog post</a>.</p>
+<p>NOTE:  the OU player is not yet accessible!!</p>
 
 
-<p><a class=embed
-href="http://podcast.open.ac.uk/oulearn/languages/spanish/podcast-l314-spanish#!fe481a4d1d">Introduction: A Buerno Puerto, on OU podcast</a>
+<h3>OU player</h3>
+Audio - Flash falls back to HTML5.
+<p><a class=embed href=
+"http://podcast.open.ac.uk/oulearn/languages/spanish/podcast-l314-spanish#!fe481a4d1d">Introduction: A Buerno Puerto, on OU podcast</a>
+
+Audio - OpenLearn/ iTunes U.
+<p><a class=embed  href=
+"http://podcast.open.ac.uk/pod/entrepreneurial-lives/#!cb127010cf">Invisible Boundaries...</a>
+
+Video - Flash falls back to HTML5.
+<p><a class=embed  href=
+"http://podcast.open.ac.uk/pod/mst209-fun-of-the-fair#!a67918b334">Circular Motion...: All the Fun of the Fair, on OU podcast</a>
 
 <!--<p><a class=embed href="http://podcast.open.ac.uk/pod/vc-message-to-staff#!746ee92293">VC message 01-02-2011, on OU podcast*</a>
 -->
 
+<h3>OU/OULDI embed</h3>
 <p><a class=embed href="http://youtube.com/watch?v=NaBBk-kpmL4">Interview with Martin Bean, on YouTube (captions)</a>
 
-<p><a class=embed href="http://lamscommunity.org/lamscentral/sequence?seq_id=1007900">Crime fighting, on LAMS</a>
+<p><a class=embed  href=
+"http://lamscommunity.org/lamscentral/sequence?seq_id=1007900">Crime fighting, on LAMS</a>
 
-<p><a class=embed href="http://spreadsheets.google.com/viewform?formkey=dDhQOXpJYkl0VzFEQnZnTkhGcF9DSFE6MQ#height=1160">CLA form, on Google Docs</a>
+<p><a class=embed  href=
+"http://spreadsheets.google.com/viewform?formkey=dDhQOXpJYkl0VzFEQnZnTkhGcF9DSFE6MQ#height=1160">CLA form, on Google Docs</a>
 
-<p><a class=embed href="http://prezi.com/zoidjousoeat/technology-for-the-classroom/">Technology, on Prezi</a>
+<p><a class=embed  href=
+"http://prezi.com/zoidjousoeat/technology-for-the-classroom">Technology, on Prezi</a>
 
+
+<p>&copy;2011 The Open University.</p>
 
 <script src=
 "http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
@@ -28,7 +49,7 @@ href="http://podcast.open.ac.uk/oulearn/languages/spanish/podcast-l314-spanish#!
 <script src="<?=site_url('scripts/jquery_oembed') ?>"></script>
 <script type="text/javascript">
   $(document).ready(function() {
-    $("a.embed"      ).oembed(); //null, { embedMethod: "auto" });
+    $("a.embed"      ).oembed(null, { embedMethod: "replace" });
     $("[rel='embed']").oembed(); //Legacy.
   });
 </script>
