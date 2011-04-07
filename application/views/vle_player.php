1eda29c8c11c6a86d7fc0a3990409a7d8006575b
Nick Freear <nfreear@yahoo.co.uk>
2011-04-07 16:26:34 +0100

    New embed/pod controller + adopted new Vle_player/ Podcast_player objects.

--- application/views/vle_player.php
+++ application/views/vle_player.php
@@ -9,42 +9,51 @@
   $player_height = $meta->height; #+ 30.
   $media_height = $meta->height - 60;
   $legacy_height = $meta->height - 30;
+  // Hold poster image for audio-player.
+  $audio_poster = null;
+
 
   $inner=$poster='';
-  if ($meta->image_url) {
-    $poster = "<img alt=\"\" src=\"$meta->image_url\" />";
-EOF;
+  if ($meta->poster_url) {
+    $poster = "<img class=\"poster\" alt=\"\" src=\"$meta->poster_url\" />";
   }
-  if ($meta->html5 && 'video' == $meta->media_type) {
+  if ($meta->media_html5 && 'video' == $meta->media_type) {
     $inner =<<<EOF
-  <video poster="$meta->image_url" width="$meta->width" height="$player_height" controls>
+  <video poster="$meta->poster_url" width="$meta->width" height="$player_height" controls>
     <source src="$meta->media_url" type='video/mp4; codecs="bogus"' /><!--Was: codecs="bogus", avc1.4D401E, mp4a.40.2 -->
     $poster<div>Your browser does not support the 'video' element.</div>
   </video>
 EOF;
   }
-  elseif ($meta->html5 && 'audio' == $meta->media_type) {
-    $inner =<<<EOF
-  $poster
-  <audio src="$meta->media_url" style="width:{$meta->width}px; height:{$player_height}px;" controls>Your browser does not support the 'audio' element.</audio>
+  elseif ($meta->media_html5 && 'audio' == $meta->media_type) {
+	$audio_poster = $poster;
+	$inner =<<<EOF
+  <audio src="$meta->media_url" style="width:{$meta->width}px; height:{$meta->object_height}px;" controls>Your browser does not support the 'audio' element.</audio>
 EOF;
   }
 ?>
 <!DOCTYPE html><html lang="en" role="application"><meta charset="utf-8" /><title><?=$meta->title ?> | OUVLE player</title>
+<!--[if lt IE 9]><?php /*http://diveintohtml5.org/semantics.html#new-elements*/ ?>
+
+<script>
+var e = ("abbr,audio,figure,time,video").split(',');
+for (var i=0; i < e.length; i++){ document.createElement(e[i]); }
+</script>
+<![endif]-->
 <style>
 html,body{margin:0; padding:0; background:#ccc;}
-._object{display:block; width:100%; height:100%;}
+object,img,audio,video{display:block;}
 </style>
 <meta name="copyright" value="&copy; 2011 The Open University" />
 
-
+<?=$audio_poster ?>
 <object tabindex="0" aria-label="Media player" type="application/x-shockwave-flash"
- width="<?=$meta->width ?>" height="<?=$legacy_height ?>"
+ width="<?=$meta->width ?>" height="<?=$meta->object_height ?>"
  data="http://learn.open.ac.uk/local/mediaplayer.swf">
  <param name="movie" value="http://learn.open.ac.uk/local/mediaplayer.swf" />
  <param name="allowfullscreen" value="true" />
  <param name="flashvars" value=
-"file=<?=$meta->media_url ?>&amp;width=<?=$meta->width ?>&amp;height=<?=$legacy_height ?>&amp;captions=<?=$meta->caption_url ?>" />
+"file=<?=$meta->media_url ?>&amp;width=<?=$meta->width ?>&amp;height=<?=$meta->object_height ?>&amp;captions=<?=$meta->caption_url ?>" />
 <?=$inner ?>
 
 </object>
