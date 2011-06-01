fd97aeee39d8c4e3fafb625473def9a830720b0c
Nick Freear <nfreear@yahoo.co.uk>
2011-06-01 16:35:58 +0100

    Add support for captions to the podcast_items Model.

--- application/models/podcast_items_model.php
+++ application/models/podcast_items_model.php
@@ -15,7 +15,8 @@ class Podcast_items_model extends CI_Model {
         $this->db_pod = $this->load->database('podcast', TRUE);
     }
 
-    public function get_item($basename, $shortcode=NULL, $transcript=FALSE) {
+    public function get_item($basename, $shortcode=NULL, $captions=FALSE) {
+
     /* SELECT * FROM podcast_items
       JOIN podcasts ON podcasts.id=podcast_items.podcast_id
       WHERE podcasts.custom_id = 'vc-message-to-staff'
@@ -28,15 +29,35 @@ class Podcast_items_model extends CI_Model {
       WHERE p.custom_id LIKE '%l314-spanish'
       AND (pi.shortcode='fe481a4d1d' OR pi.filename='l314audio1.mp3');
 SQL;
+        // This doesn't work!
+        $where_podcast_items = 'podcast_items.shortcode';
+        if (FALSE !== strpos($shortcode, '.')) {
+            $where_podcast_items = 'podcast_items.filename';
+        }
+        $sql_cc =<<<SQL
+  SELECT
+  podcasts.title AS pod_title,podcasts.summary AS pod_summary,podcasts.*,podcast_items.*,pim.media_type AS pim_type,pim.filename AS pim_filename
+	FROM podcast_items
+	JOIN podcasts ON podcasts.id=podcast_items.podcast_id
+	LEFT JOIN podcast_item_media AS pim ON pim.podcast_item=podcast_items.id
+  WHERE(
+	pim.media_type='cc-dfxp'  -- IS NOT NULL
+	AND podcasts.custom_id = '$basename'
+	AND $where_podcast_items='$shortcode' )
+  OR( podcasts.custom_id = '$basename'
+	AND $where_podcast_items='$shortcode' )
+SQL;
 
+        // This works!
         $select = 'podcasts.title AS pod_title, podcasts.summary AS pod_summary, podcasts.*,podcast_items.*';
-        if ($transcript) {
-            $select .= ', podcast_item_media.filename AS pim_filename';
+        if ($captions) {
+            $select .= ', podcast_item_media.filename AS pim_filename, podcast_item_media.media_type AS pim_type';
         }
         $this->db_pod->select($select);
         $this->db_pod->join('podcasts', 'podcasts.id=podcast_items.podcast_id');
-        if ($transcript) {
-            $this->db_pod->join('podcast_item_media', 'podcast_item_media.podcast_item=podcast_items.id');
+        if ($captions) {
+            $this->db_pod->join('podcast_item_media', 'podcast_item_media.podcast_item=podcast_items.id', 'left');
+            $this->db_pod->order_by('pim_type', 'desc');
         }
         $this->db_pod->where('podcasts.custom_id', $basename);      #'l314-spanish');
         if (FALSE !== strpos($shortcode, '.')) { #(FALSE !== strpos($basename, '.m') && !$shortcode) {
@@ -46,11 +67,20 @@ SQL;
         }
         $query = $this->db_pod->get('podcast_items'); #AS pi');
 
+        //$query = $this->db_pod->query($sql_cc);
+
         #echo $this->db_pod->last_query()."\n<br>\n";
 
-        #if $transcript - multiple results! - Post-process
-
         $result = $query->result();
+
+        # Captions - multiple results! - Post-process
+        if ($result && $captions) {
+            foreach ($result as $res) {
+                if ('cc-dfxp'==$res->pim_type) {
+                    return $res;
+                }
+            }
+        }
         if ($result) {
           return $result[0];
         }
