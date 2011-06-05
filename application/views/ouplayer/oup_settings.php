a017dd44105e925a2132221937a41a5f24db2515
Nick Freear <nfreear@yahoo.co.uk>
2011-06-05 20:51:49 +0100

    Lots of mods. to player-settings view...
    * Rename the embed-code IDs & classes so they are unique, and match the behaviour Javascript,
    * Using OUP_PARAM_THEME for embed code etc.

--- application/views/ouplayer/oup_settings.php
+++ application/views/ouplayer/oup_settings.php
@@ -11,22 +11,26 @@
   <img class="ou-home logo" alt="Open University logo" src="<?=site_url('assets/0.gif') ?>" height="38" width="32" />
   <ul class="mediatitle">
   <li><h1><?=$meta->title; /*substr_replace($meta->title, '…', 62)*/ ?></h1></li>
-  <li><?php if($meta->summary): ?><span class="summary"><?=substr_replace($meta->summary, '…', 100) ?></span><?php endif; ?>
-  <?php if($meta->_related_url)echo anchor($meta->_related_url, $meta->_related_text, array('class'=>'rel-2','target'=>'_blank','title'=>t('New window'))); ?></li>
+  <li><?php if($meta->summary): ?><span class="summary"><?=substr_replace($meta->summary, '…', 95) ?></span><?php endif; ?>
+  <?php if($meta->_related_url){
+    #$rel_text = 'video'==$meta->media_type ? $meta->_related_text : substr_replace($meta->_related_text, '…', 55);
+    echo anchor($meta->_related_url, $meta->_related_text, array('class'=>'rel-2','target'=>'_blank','title'=>t('New window')));
+  } ?></li>
   </li></ul>
 
 
 <?php
   // Embed code - uses jQuery-oEmbed plugin.
   $em_title = substr_replace($meta->title, '…', 36);
+  $param_theme = OUP_PARAM_THEME;
   $jq_oembed=<<<EOF
-<a class="embed" href="$meta->_short_url">$em_title</a>
+<!--Copy and paste--><a class="embed" href="$meta->_short_url">$em_title</a>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
 <script src="http://embed.open.ac.uk/scripts/jquery.oembed.js"></script>
 <script>
 $(document).ready(function(){
-  $("a.embed").oembed(null,{oupodcast:{_theme:"$theme"}});
+$("a.embed").oembed(null,{oupodcast:{{$param_theme}:"$theme"}});
 });
 </script>
 EOF;
@@ -38,15 +42,15 @@ EOF;
   <a class="about" href="#about/TODO" title="<?=t('New window') ?>"><span><?=t('About') ?></span></a>
 <?php /*<button class="decreasesize" aria-label="<?=t('Decrease text size') ?>"><span>-A</span></button>
   <button class="increasesize" aria-label="<?=t('Increase text size') ?>"><span>A+</span></button>*/ ?>
-  <label role="button" class="themeoption" for="theme-menu" aria-label="Style changer"><span><?=t('Theme') ?><?php /*<img class="styleicon" alt="Style icon" src="a6../styleicon.jpg" height="16" width="16">Style*/ ?></span></label>
-  <select id="theme-menu" name="_theme">
+  <label role="button" class="themeoption" for="theme-menu" title="<?=t('Choose the theme') ?>"><span><?=t('Theme') ?><?php /*<img class="styleicon" alt="Style icon" src="a6../styleicon.jpg" height="16" width="16">Style*/ ?></span></label>
+  <select id="theme-menu" name="<?=OUP_PARAM_THEME ?>">
     <option value="ouice-dark">OUICE Dark</option>
     <option value="ouice-bold" selected>OUICE Bold</option>
   </select>
   </div>
   <div class="col2">
-  <label role="button" class="embed" for="embed-code"><span><?=t('Embed code') ?></span></label></a>
-  <textarea id="embed-code" readonly title="Javascript-based embed (oEmbed)"><?=str_replace('<','&lt;', $jq_oembed) ?></textarea>
+  <label role="button" class="embed" for="embed-code" title="<?=t('Embed on other sites') ?>"><span><?=t('Embed code') ?></span></label></a>
+  <textarea id="embed-code" class="emcode-opt" readonly title="Javascript-based embed (oEmbed)"><?=str_replace('<','&lt;', $jq_oembed) ?></textarea>
   <a class="embed-opt" href="#embed/TODO" title="<?=t('New window') ?>"><span><?=t('More embeds…') ?></span></a>
 <?php /*<button class=""><span>Option</span></button>
   <button class=""><span>Text</span></button>
@@ -55,6 +59,7 @@ EOF;
   <div class="col3">
   <a class="media-url" href="<?=$meta->media_url ?>" target="_blank" title="<?=t('New window') ?>"><span><?=t('Download media') ?></span></a>
   <a class="script-pdf" href="<?=$meta->transcript_url ?>" target="_blank" title="<?=t('New window: PDF') ?>"><span><?=t('Download transcript') ?></span></a>
+<?php ///Translators: 'View on OU Podcasts web site' ?>
   <a class="short-url" rel="bookmark" href="<?=$meta->_short_url ?>" target="_blank" title="<?=t('New window: perma-link') ?>"><span><?=t('View on Podcasts') ?></span></a>
   </div>
   </div>
@@ -66,12 +71,12 @@ EOF;
   <button class="more-close" title="<?=t('Close settings') ?>"><span>X</span></button>
   <a class="help" href="#help/TODO"  title="<?=t('New window') ?>"><span><?=t('Player help') ?></span></a>
   <a class="about" href="#about/TODO" title="<?=t('New window') ?>"><span><?=t('About the player') ?></span></a>
-  <?php /*<a class="embed" href="#embed-code">*/ ?><label class="embed" for="embed-code"><span><?=t('Embed code') ?></span></label></a>
-  <textarea id="embed-code" readonly title="Javascript-based embed (oEmbed)"><?=str_replace('<','&lt;', $jq_oembed) ?></textarea>
+  <?php /*<a class="embed" href="#embed-code">*/ ?><label class="embed" for="emcode-more"><span><?=t('Embed code') ?></span></label></a>
+  <textarea id="emcode-more" class="emcode-more" readonly title="Javascript-based embed (oEmbed)"><?=str_replace('<','&lt;', $jq_oembed) ?></textarea>
   <a class="embed-opt" href="#embed/TODO"><span><?=t('More embed options') ?></span></a>
   <a class="media-url" href="<?=$meta->media_url ?>" target="_blank" title="<?=t('New window') ?>"><span><?=t('Download media') ?></span></a>
   <a class="script-pdf" href="<?=$meta->transcript_url ?>" target="_blank" title="<?=t('New window: PDF') ?>"><span><?=t('Download transcript') ?></span></a>
-  <a class="short-url" rel="bookmark" href="<?=$meta->_short_url ?>" target="_blank" title="<?=t('New window: perma-link') ?>"><span><?=t('View on OU podcasts') ?></span></a>
+  <a class="short-url" rel="bookmark" href="<?=$meta->_short_url ?>" target="_blank" title="<?=t('New window: perma-link') ?>"><span><?=t('View on Podcasts site') ?></span></a>
 </div>
 
 
