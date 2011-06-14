22d2aff65ea437ebd1c70db52cb6e145ff82f01f
Nick Freear <nfreear@yahoo.co.uk>
2011-06-14 16:55:38 +0100

    Adding the 'theme' parameter to the jquery-oembed call + ...
    * Switched from scripts/jquery_oembed to scripts/jquery.oembed.js
    * Switched to the student-experiences video.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -17,13 +17,15 @@ Audio - Flash falls back to HTML5.
 <p><a class=embed href=
 "http://podcast.open.ac.uk/oulearn/languages/spanish/podcast-l314-spanish#!fe481a4d1d">Introduction: A Buerno Puerto, on OU podcast</a>
 
-Audio - OpenLearn/ iTunes U.
+
+<p><a class=embed href="http://podcast.open.ac.uk/pod/student-experiences#!db6cc60d6b">Student Experiences</a>
+<!--<p>Audio - OpenLearn/ iTunes U.
 <p><a class=embed  href=
 "http://podcast.open.ac.uk/pod/entrepreneurial-lives/#!cb127010cf">Invisible Boundaries..: Entrepreneurial Lives, on OU podcast</a>
+-->
 
-Video - Flash falls back to HTML5.
-<p><a class=embed  href=
-"http://podcast.open.ac.uk/pod/mst209-fun-of-the-fair#!a67918b334">Circular Motion...: All the Fun of the Fair, on OU podcast</a>
+<p>Video - Flash falls back to HTML5.
+<p><a class=embed  href="http://podcast.open.ac.uk/pod/mst209-fun-of-the-fair#!a67918b334">Circular Motion...: All the Fun of the Fair, on OU podcast</a>
 
 
 <h3>OU/OULDI embed</h3>
@@ -41,15 +43,13 @@ Video - Flash falls back to HTML5.
 
 <p>&copy;2011 The Open University.</p>
 
-<script src=
-"http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
-<!--<script src="scripts/jquery.oembed.js"></script>-->
-<script src="<?=site_url('scripts/jquery_oembed') ?>"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<script src="<?=site_url('scripts/jquery.oembed.js') ?>"></script>
 <script type="text/javascript">
   $(document).ready(function() {
-    $("a.embed"      ).oembed();<?php /*null, { embedMethod: "replace" });*/ ?>
+    $("a.embed"    ).oembed(null, {'oupodcast':{'<?=OUP_PARAM_THEME ?>':'<?=$req->theme ?>'}});<?php /*null, { embedMethod: "replace" });*/ ?>
 
-    $("[rel='embed']").oembed(); //Legacy.
+    $("[rel=embed]").oembed(); //Legacy.
   });
 </script>
 
