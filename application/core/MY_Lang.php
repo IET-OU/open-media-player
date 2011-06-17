9c47cb76991c7f322bb8ee9abdd7a80080f0eb1f
Nick Freear <nfreear@yahoo.co.uk>
2011-06-17 15:48:37 +0100

    Adding 'Accept-Language' functionality to MY_Lang::initialize - from CloudEngine/Cloudworks.

--- application/core/MY_Lang.php
+++ application/core/MY_Lang.php
@@ -26,13 +26,59 @@ class My_Lang extends CI_Lang {
   */
   public function initialize() {
     $CI =& get_instance();
-    $lang = $CI->input->get('lang');
-    $lang = preg_match('/^[a-z]{2,3}([-_][a-zA-Z]{2,4})?$/', $lang) ? $lang : 'en';
-    $lang = str_replace('_', '-', $lang);
-    $this->_lang_ui = $lang;
+    #$lang = $CI->input->get('lang');
+    #$lang = preg_match('/^[a-z]{2,3}([-_][a-zA-Z]{2,4})?$/', $lang) ? $lang : 'en';
+    #$lang = str_replace('_', '-', $lang);
 
+	# Load locales from config/embed_config.php file.
     $this->_locales = $CI->config->item('locales');
+	$_lang = 'en'; #$this->_lang_ui;
+	$method= 'non';
 
+	# 1. Content negotiation, using 'Accept-Language' header.
+	$CI->load->library('user_agent');
+
+	# Order is significant :(
+	foreach ($this->_locales as $lang => $item) {
+		if ($CI->agent->accept_lang(strtolower($lang))) {
+			# If it's an alias, follow it.
+			$_lang = is_string($item) ? $item : $lang;
+			$method= 'ACC';
+			break;
+		}
+	}
+
+	# 2. If there's a COOKIE, use that.
+	#$lc = $this->CI->input->cookie('language', FALSE);
+	#...
+
+	# 3. If it's POST[lang], set a cookie (or session?)
+	#$lp = $this->CI->input->post('lang', FALSE);
+	#...
+
+	# 4. Finally, if there's a GET[lang], then override (unless a cookie was just set?)
+	$lg = strtolower(str_replace('_', '-', $CI->input->get('lang', FALSE)));
+	if ($lg && isset($this->_locales[$lg])) {
+		if (is_string($this->_locales[$lg])) {
+			$lg = $this->_locales[$lg];
+		}
+		if ('CKP'==$method) {
+			$method = 'CKI';
+		} else {
+			$_lang = $lg;
+			$method = 'GET';
+		}
+	}
+
+	$CI->_log('debug', "My_Lang: $_lang | how=$method | ".$_SERVER['REQUEST_URI']
+		                ." | ".$CI->agent->agent_string()." | "
+		                ." | ".$_SERVER['REMOTE_ADDR']); #$this->accept_lang()
+
+	$this->lang_ui = $_lang;
+	$this->_load_gettext($_lang);
+  }
+
+  protected function _load_gettext($lang) {
     $path = APPPATH."language/$lang.po";  #$lang/application.po;
 
     if (file_exists($path)) {
