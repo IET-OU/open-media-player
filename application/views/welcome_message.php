ad57b9d7f0cdddd99290112091ba08bde3b94dc1
Nick Freear <nfreear@yahoo.co.uk>
2011-08-31 12:46:34 +0100

    Updating  text in the welcome message/home/demo view.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html><html lang="en"><meta charset="utf-8"/><title>*OU player/ OU embed - Demonstrations</title>
+<!DOCTYPE html><html lang="en"><meta charset="utf-8"/><title>*OU player/ OU embed - Beta Demonstrations</title>
 
 <meta name="ROBOTS" content="noindex,nofollow" />
 
@@ -6,10 +6,12 @@
 
 <h1>OU player</h1> <h2>Prototype demonstrations</h2>
 
-<p>These are early prototypes for the <abbr title="The Open University">OU</abbr> player/<abbr title=
+<p>These are Beta demonstrations for the <abbr title="The Open University">OU</abbr> player/<abbr title=
 "Open University Learning Design Initiative, including Cloudworks">OULDI</abbr> embed projects. Here are <a href=
 "http://freear.org.uk/content/ou-media-player-project">introductory blog</a>  <a href="http://freear.org.uk/content/ou-embed-proposal">posts</a>.</p>
-<p>NOTE:  the OU player is not yet accessible!!</p>
+
+<p>NOTE:  these demos use the <a href="<?=site_url('scripts/jquery.oembed.js') ?>">jquery-oembed plugin</a>.
+<p>NOTE:  the OU player is now fairly accessible. Feedback to <a href="mailto:N.D.Freear+@+open.ac.uk?subject=OU+player">N.D.Freear+@+open.ac.uk</a></p>
 
 
 <h3>OU player</h3>
@@ -23,6 +25,7 @@ Audio - Flash falls back to HTML5.
 <p><a class=embed  href=
 "http://podcast.open.ac.uk/pod/entrepreneurial-lives/#!cb127010cf">Invisible Boundaries..: Entrepreneurial Lives, on OU podcast</a>
 -->
+<br /><a href="http://embed.open.ac.uk/embed/pod/student-experiences/db6cc60d6b?theme=ouice-light">iframe</a>
 
 <p>Video - Flash falls back to HTML5.
 <p><a class=embed  href="http://podcast.open.ac.uk/pod/mst209-fun-of-the-fair#!a67918b334">Circular Motion...: All the Fun of the Fair, on OU podcast</a>
@@ -45,7 +48,7 @@ Audio - Flash falls back to HTML5.
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
 <script src="<?=site_url('scripts/jquery.oembed.js') ?>"></script>
-<script type="text/javascript">
+<script>
   $(document).ready(function() {
     $("a.embed"    ).oembed(null, {'oupodcast':{'<?=OUP_PARAM_THEME ?>':'<?=$req->theme ?>'}});<?php /*null, { embedMethod: "replace" });*/ ?>
 
