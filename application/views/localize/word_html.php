796dbe286c43751f2cb5ab54814e953a212a8d39
Nick Freear <nfreear@yahoo.co.uk>
2011-12-08 17:09:14 +0000

    Bug #1278, Initial word_html view - for easy translation using MS Word - i18n.

---
+++ application/views/localize/word_html.php
@@ -0,0 +1,178 @@
+<html>
+
+<head>
+<meta name=Title content="">
+<meta name=Keywords content="">
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<meta name=ProgId content=Word.Document>
+<meta name=Generator content="Microsoft Word 2008">
+<meta name=Originator content="Microsoft Word 2008">
+<link rel=File-List href="ouplayer-lang_files/filelist.xml">
+<style>
+<!--
+ /* Font Definitions */
+@font-face
+	{font-family:Cambria;
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:"ヒラギノ明朝 ProN W3";
+	panose-1:0 0 0 0 0 0 0 0 0 0;}
+@font-face
+	{font-family:"Heiti TC Light";
+	panose-1:2 0 0 0 0 0 0 0 0 0;}
+ /* Style Definitions */
+p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin:0cm;
+	margin-bottom:.0001pt;
+	font-size:12.0pt;
+	font-family:"Times New Roman";
+	mso-ascii-font-family:Cambria;
+	mso-fareast-font-family:Cambria;
+	mso-hansi-font-family:Cambria;
+	mso-bidi-font-family:"Times New Roman";}
+pre
+	{margin:0cm;
+	margin-bottom:.0001pt;
+	font-size:10.0pt;
+	font-family:Courier;
+	mso-fareast-font-family:Cambria;
+	mso-bidi-font-family:Courier;}
+span.HTMLPreformattedChar
+	{mso-ansi-font-size:10.0pt;
+	mso-bidi-font-size:10.0pt;
+	font-family:Courier;
+	mso-ascii-font-family:Courier;
+	mso-hansi-font-family:Courier;
+	mso-bidi-font-family:Courier;}
+@page Section1
+	{size:595.0pt 842.0pt;
+	margin:72.0pt 90.0pt 72.0pt 90.0pt;
+	mso-paper-source:0;}
+div.Section1
+	{page:Section1;}
+-->
+</style>
+<style>
+ /* Style Definitions */
+table.MsoNormalTable
+	{mso-para-margin:0cm;
+	mso-para-margin-bottom:.0001pt;
+	font-size:10.0pt;
+	font-family:"Times New Roman";
+	mso-ascii-font-family:Cambria;
+	mso-hansi-font-family:Cambria;}
+</style>
+</head>
+
+<body lang=EN-US style='tab-interval:36.0pt'>
+
+<div class=Section1>
+
+<p class=MsoNormal>Language pack example.</p>
+
+<p class=MsoNormal>lang: zh-CN</p>
+
+<p class=MsoNormal>&nbsp;</p>
+
+<p class=MsoNormal style='tab-stops:45.8pt 91.6pt 137.4pt 183.2pt 229.0pt 274.8pt 320.6pt 366.4pt 412.2pt 458.0pt 503.8pt 549.6pt 595.4pt 641.2pt 687.0pt 732.8pt'><span
+style='font-size:10.0pt;font-family:"ヒラギノ明朝 ProN W3";mso-bidi-font-family:"ヒラギノ明朝 ProN W3"'>播放</span><span
+style='font-size:10.0pt;font-family:"Heiti TC Light";mso-bidi-font-family:"Heiti TC Light"'>视频</span></p>
+
+<p class=MsoNormal>&nbsp;</p>
+
+<p class=MsoNormal>[End.]</p>
+
+</div>
+
+</body>
+
+</html>
+<html>
+
+<head>
+<meta name=Title content="">
+<meta name=Keywords content="">
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<meta name=ProgId content=Word.Document>
+<meta name=Generator content="Microsoft Word 2008">
+<meta name=Originator content="Microsoft Word 2008">
+<link rel=File-List href="ouplayer-lang_files/filelist.xml">
+<style>
+<!--
+ /* Font Definitions */
+@font-face
+	{font-family:Cambria;
+	panose-1:2 4 5 3 5 4 6 3 2 4;}
+@font-face
+	{font-family:"ヒラギノ明朝 ProN W3";
+	panose-1:0 0 0 0 0 0 0 0 0 0;}
+@font-face
+	{font-family:"Heiti TC Light";
+	panose-1:2 0 0 0 0 0 0 0 0 0;}
+ /* Style Definitions */
+p.MsoNormal, li.MsoNormal, div.MsoNormal
+	{margin:0cm;
+	margin-bottom:.0001pt;
+	font-size:12.0pt;
+	font-family:"Times New Roman";
+	mso-ascii-font-family:Cambria;
+	mso-fareast-font-family:Cambria;
+	mso-hansi-font-family:Cambria;
+	mso-bidi-font-family:"Times New Roman";}
+pre
+	{margin:0cm;
+	margin-bottom:.0001pt;
+	font-size:10.0pt;
+	font-family:Courier;
+	mso-fareast-font-family:Cambria;
+	mso-bidi-font-family:Courier;}
+span.HTMLPreformattedChar
+	{mso-ansi-font-size:10.0pt;
+	mso-bidi-font-size:10.0pt;
+	font-family:Courier;
+	mso-ascii-font-family:Courier;
+	mso-hansi-font-family:Courier;
+	mso-bidi-font-family:Courier;}
+@page Section1
+	{size:595.0pt 842.0pt;
+	margin:72.0pt 90.0pt 72.0pt 90.0pt;
+	mso-paper-source:0;}
+div.Section1
+	{page:Section1;}
+-->
+</style>
+<style>
+ /* Style Definitions */
+table.MsoNormalTable
+	{mso-para-margin:0cm;
+	mso-para-margin-bottom:.0001pt;
+	font-size:10.0pt;
+	font-family:"Times New Roman";
+	mso-ascii-font-family:Cambria;
+	mso-hansi-font-family:Cambria;}
+</style>
+</head>
+
+<body lang=EN-US style='tab-interval:36.0pt'>
+
+<div class=Section1>
+
+<p class=MsoNormal>Language pack example.</p>
+
+<p class=MsoNormal>lang: zh-CN</p>
+
+<p class=MsoNormal>&nbsp;</p>
+
+<p class=MsoNormal style='tab-stops:45.8pt 91.6pt 137.4pt 183.2pt 229.0pt 274.8pt 320.6pt 366.4pt 412.2pt 458.0pt 503.8pt 549.6pt 595.4pt 641.2pt 687.0pt 732.8pt'><span
+style='font-size:10.0pt;font-family:"ヒラギノ明朝 ProN W3";mso-bidi-font-family:"ヒラギノ明朝 ProN W3"'>播放</span><span
+style='font-size:10.0pt;font-family:"Heiti TC Light";mso-bidi-font-family:"Heiti TC Light"'>视频</span></p>
+
+<p class=MsoNormal>&nbsp;</p>
+
+<p class=MsoNormal>[End.]</p>
+
+</div>
+
+</body>
+
+</html>
