284563d2cf439f3fce83a28964f094450546467e
Nick Freear <nfreear@yahoo.co.uk>
2012-04-15 23:53:32 +0100

    Re-working Base_service::_http_request_curl to use Http library..
    * Adding curl_setopt(.., CURLOPT_MAXREDIRS, ..), etc.
    * Adding Http::_prepare_request based on the Drupal function,
    * http://api.drupal.org/api/drupal/core%21includes%21common.inc/function/drupal_http_request/8

--- application/libraries/base_service.php
+++ application/libraries/base_service.php
@@ -21,32 +21,8 @@ abstract class Base_service implements iService {
   }
 
   protected function _http_request_curl($url, $spoof=TRUE) {
-    if (!function_exists('curl_init'))  die('Error, cURL is required.');
-
-    $ua = 'My client/0.1 (PHP/cURL)';
-    if ($spoof) {
-       $ua="Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-GB; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3";
-    }
-
-    $h_curl = curl_init($url);
-    curl_setopt($h_curl, CURLOPT_USERAGENT, $ua);
-    if (!$spoof) {
-      curl_setopt($h_curl, CURLOPT_REFERER,   'http://example.org');
-    }
-	$http_proxy = $this->CI->config->item('http_proxy');
-	if ($http_proxy) {
-	  curl_setopt($h_curl, CURLOPT_PROXY, $http_proxy);
-	}
-    curl_setopt($h_curl, CURLOPT_RETURNTRANSFER, TRUE);
-    $result = array('data' => curl_exec($h_curl));
-    if ($errno = curl_errno($h_curl)) {
-      //Error. Quietly log?
-      $this->CI->_log('error', "cURL $errno, ".curl_error($h_curl)." GET $url");
-      #$this->CI->firephp->fb("cURL $errno", "cURL error", "ERROR");
-    }
-    $result['info'] = curl_getinfo($h_curl);
-    $result['success'] = ($result['info']['http_code'] < 300);
-    return (object) $result;
+    $this->CI->load->library('http');
+    return $this->CI->http->request($url, $spoof);
   }
   
   protected function _http_request_json($url, $spoof=TRUE) {
--- application/libraries/http.php
+++ application/libraries/http.php
@@ -9,13 +9,35 @@
 
 class Http {
 
-  public function request($url, $spoof=TRUE) {
-    return $this->_http_request_curl($url, $spoof);
+  public function request($url, $spoof=TRUE, $options=array()) {
+    $result = $this->_prepare_request($url, $spoof, $options);
+
+    return $this->_http_request_curl($url, $spoof, $options, $result);
   }
 
 
-  protected function _http_request_curl($url, $spoof=TRUE) {
-    if (!function_exists('curl_init'))  die('Error, cURL is required.');
+  /** Prepare the HTTP request.
+  */
+#http://api.drupal.org/api/drupal/core%21includes%21common.inc/function/drupal_http_request/8
+  protected function _prepare_request($url, $spoof, &$options) {
+    $result = new stdClass();
+
+    // Parse the URL and make sure we can handle the schema.
+    $uri = @parse_url($url);
+
+    if ($uri == FALSE) {
+      $result->error = 'unable to parse URL';
+      $result->code = -1001;
+      return $result;
+    }
+
+    if (!isset($uri['scheme'])) {
+      $result->error = 'missing schema';
+      $result->code = -1002;
+      return $result;
+    }
+
+    #timer_start(__FUNCTION__);
 
     $ua = 'OU Player/0.9 (PHP/cURL)';
     if ($spoof) {
@@ -24,25 +46,52 @@ class Http {
        #$ua="Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-GB; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3";
     }
 
+    // Merge the default options.
+    $options += array(
+      'headers' => array(),
+      'method' => 'GET',
+      'data' => NULL,
+      'max_redirects' => 2,  #3,
+      'timeout' => 15.0,  #30.0 seconds,
+      'context' => NULL,
+
+      'ua' => $ua,
+    );
+    return $result;
+  }
+
+
+  /** Perform the HTTP request using cURL.
+  */
+  protected function _http_request_curl($url, $spoof, $options, $result) {
+    if (!function_exists('curl_init'))  die('Error, cURL is required.');
+
     $h_curl = curl_init($url);
-    curl_setopt($h_curl, CURLOPT_USERAGENT, $ua);
+    curl_setopt($h_curl, CURLOPT_USERAGENT, $options['ua']);
     if (!$spoof) {
       curl_setopt($h_curl, CURLOPT_REFERER, base_url());
     }
 
-	$http_proxy = config_item('http_proxy'); //$this->CI->config->item('http_proxy');
+    curl_setopt($h_curl, CURLOPT_AUTOREFERER, TRUE);
+    curl_setopt($h_curl, CURLOPT_MAXREDIRS, $options['max_redirects']);
+    curl_setopt($h_curl, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($h_curl, CURLOPT_TIMEOUT, $options['timeout']);
+
+	$this->CI =& get_instance();
+
+	$this->CI->config->item('http_proxy');
 	if ($http_proxy) {
 	  curl_setopt($h_curl, CURLOPT_PROXY, $http_proxy);
 	}
     curl_setopt($h_curl, CURLOPT_RETURNTRANSFER, TRUE);
-    $result = array('data' => curl_exec($h_curl));
+    $result->data = curl_exec($h_curl);
     if ($errno = curl_errno($h_curl)) {
       //Error. Quietly log?
       $this->CI->_log('error', "cURL $errno, ".curl_error($h_curl)." GET $url");
       #$this->CI->firephp->fb("cURL $errno", "cURL error", "ERROR");
     }
-    $result['info'] = curl_getinfo($h_curl);
-    $result['success'] = ($result['info']['http_code'] < 300);
+    $result->info = curl_getinfo($h_curl);
+    $result->success = ($result->info['http_code'] < 300);
     return (object) $result;
   }
   
