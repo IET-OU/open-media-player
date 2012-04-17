2f648a77f397b91a03e6a0d40f3ad2501b0c3403
Nick Freear <nfreear@yahoo.co.uk>
2012-04-17 23:25:55 +0100

    Extending the Build controller to 'support' CSS..
    * With <a> links to two online YUI compressors.

--- application/controllers/build.php
+++ application/controllers/build.php
@@ -8,39 +8,65 @@
 *
 * @link http://closure-compiler.appspot.com/home
 * (http://www.minifyjs.com/javascript-compressor/)
+* (http://www.lotterypost.com/css-compress.aspx)
 * @copyright 2012 The Open University.
 * @author N.D.Freear, 17 April 2012.
 */
 
 class Build extends MY_Controller {
 
-  public function theme($theme_name = 'ouplayer-base', $optimizations = 'simple') {	
-	$theme_name = strtolower(str_replace('-', '_', $theme_name));
-	$optimizations = strtoupper($optimizations);
+  public function theme($theme_name = 'oup-light', $optimizations = 'simple', $output = 'ouplayer.min') {	
+    $theme_name = strtolower(str_replace('-', '_', $theme_name));
+    $optimizations = strtoupper($optimizations);
 
-	$this->load->theme($theme_name);
 
-	$mep_scripts = $this->theme->javascripts;
+    $this->load->theme($theme_name);
+
     $base_url = base_url() .'application/';
     $rand = rand(0, 100);
 
-    $closure = <<<EOF
+    $closure_template = <<<EOF
 <!doctype html><title>*Closure compiler script | OU Player</title>
 <a href="http://closure-compiler.appspot.com/home">closure-compiler.appspot.com/home</a>
 <pre>
 // ==ClosureCompiler==
-// @output_file_name mediaelement-and-ou-player.min.js
+// @output_file_name $output.js
 // @compilation_level {$optimizations}_OPTIMIZATIONS
 //__URLS__// ==/ClosureCompiler==
+</pre>
 
+EOF;
+
+    $url_list = '';
+    foreach ($this->theme->javascripts as $script) {
+      $url_list .= "// @code_url $base_url$script?r=$rand".PHP_EOL;
+    }
+
+    echo str_replace('//__URLS__', $url_list, $closure_template);
+
+    $this->_css($output);
+  }
+
+
+  protected function _css($output) {
+    $base_url = base_url() .'application/';
+    $rand = rand(0, 100);
+
+    $css_template = <<<EOF
+
+<a href="http://ganquan.info/yui/">ganquan.info/yui</a> | <a href="http://refresh-sf.com/yui/">refresh-sf.com/yui</a>
+<pre>
+// ==CssCompiler==
+// @output_file_name $output.css
+//__URLS__// ==/CssCompiler==
 </pre>
 EOF;
 
     $url_list = '';
-    foreach ($mep_scripts as $script) {
-      $url_list .= "// @code_url $base_url$script?r=$rand".PHP_EOL;
+    foreach ($this->theme->styles as $style) {
+      $url_list .= "// @code_url $base_url$style?r=$rand".PHP_EOL;
     }
 
-    echo str_replace('//__URLS__', $url_list, $closure);
+    echo str_replace('//__URLS__', $url_list, $css_template);
   }  
 }
