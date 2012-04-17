147e29384a4a3fc6cb0baf05273ca144bc471a51
Nick Freear <nfreear@yahoo.co.uk>
2012-04-17 16:48:25 +0100

    Bug #1323, Re-work of ignore-color Javascript feature, plus basic CSS - accessibility fix.

--- application/themes/ouplayer_base/js/mep-oup-feature-ignore-color.js
+++ application/themes/ouplayer_base/js/mep-oup-feature-ignore-color.js
@@ -1,5 +1,5 @@
 /**
-* OU player: Detect high contrast/ ignore colours browser/OS mode.
+* OU player: Detect high contrast/ ignore colours browser/OS modes - accessibility.
 * Copyright 2012 The Open University.
 * Author: Nick Freear, 2012-04-17
 * Author: John Snyder, 2009-11-04
@@ -12,30 +12,40 @@ http://hardlikesoftware.com/projects/HCMtest.html
 */
 (function($) {
 
-$(document).ready(function () {
-
+function oup_detect_ignore_color(){
   /*
     The following code detects high contrast mode.
     It works because in high contrast mode the reported background color will not
     be the same as it was explicitly set. 
   */
-  // add an element with explicit background color set
-  $("body").append("<p id='hcmtest' style='position:absolute;top:0;left:-99999px;background-color:#878787;'>T</p>");
-  var testcolor = $("#hcmtest").css("background-color").toLowerCase(); // now get the background color
-  $("#hcmtest").remove(); // no longer needed
-  // different browsers return the color in different ways
-  if (testcolor != "#878787" && testcolor != "rgb(135, 135, 135)") {
-    // make any specific changes for high contrast mode here
-    $("#msg").html("<p>FYI: In high contrast mode.</p>");
+  var body = $("body")
+  , hcmclass = "no-ignore-color"
+  // (No need to append or remove the element.)
+  // add an element with explicit background color set, and now get the background color.
+  , hcm = $("<p style='position:absolute;top:0;left:-999px;background-color:#878787;'>T</p>")
+  , testcolor = hcm.css("background-color").toLowerCase()
+  ;
+  // different browsers return the color in different ways - beware spaces!
+  mejs.isIgnoreColor = (testcolor != "#878787" && testcolor != "rgb(135, 135, 135)");
+  if (mejs.isIgnoreColor) {
+    hcmclass = "ignore-color";
+  }
+  body.removeClass('ignore-color no-ignore-color').addClass(hcmclass);
+  $.log('> '+hcmclass);
+};
+// Resize event seems to be fired on change to/from Windows high-contrast.
+$(window).resize(oup_detect_ignore_color);
+
+$(document).ready(function(){
+  oup_detect_ignore_color();
+
+  //? http://darcyclarke.me/development/detect-attribute-changes-with-jquery/
+  if (typeof $.browser!='undefined' && $.browser.mozilla){
+	//$(document).bind("DOMAttrModified", oup_detect_ignore_color);
   }
 
-  /*
-  $(".disclose1, .disclose2").click(function() {
-    $(this).toggleClass("closed").parent().next("p").eq(0).toggleClass("closed");
-  });
-
-  fixIcons($("#case2"));
-  */
+  // Experimental: works when MSIE/Mozilla enter 'ignore colour' mode (and Mozilla leaves).
+  setInterval(oup_detect_ignore_color, 5000);
 });
-	
+
 })(mejs.$);
