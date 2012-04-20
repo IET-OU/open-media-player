d167658b8320b72f3d07449d0085e38e3eb7deee
Nick Freear <nfreear@yahoo.co.uk>
2012-04-20 15:10:54 +0100

    Modifying the 'international' view in the Player base theme...
    * ..So that language strings aren't output for English in the Player Javascript config.
    * They aren't required as they duplicate the strings in the Javascript libraries/features.

--- application/themes/ouplayer_base/views/oup-mep-international.php
+++ application/themes/ouplayer_base/views/oup-mep-international.php
@@ -1,43 +1,49 @@
 <?php
 /**
-* Internationalization/Localization.
+* Internationalization/ Localization.
+* Note, this view is output in the middle of a Javascript object!
 *
 * See,
 *   ouplayer/app/core/MY_Lang.php
 *   ouplayer/app/language/ouplayer.pot.po
 */
+$lang_ui = $this->lang->lang_code();
 
+?>
+  startLanguage:"en", langUI:"<?=$lang_ui ?>", tracksText:"<?=t('Captions') ?>",
+<?php
+if ('en' != $lang_ui && 'en-gb' != $lang_ui && 'en-us' != $lang_ui):
 
-$oup_mep_texts = array(
-  'startLanguage' => 'en', //$params->lang,
+  $oup_mep_texts = array(
+    //'startLanguage' => 'en', //$params->lang,
 
-  'controlsText' => t('Player controls'),
-  '_loadingText' => t('Loading, please wait'),
-  '_relatedText' => t('Related link: %s'),
+    'controlsText' => t('Player controls'),
+    '_loadingText' => t('Loading, please wait'),
+    '_relatedText' => t('Related link: %s'),
 
-  'playText' => t('Play'),
-  'pauseText' => t('Pause'),
+    'playText' => t('Play'),
+    'pauseText' => t('Pause'),
 
-  'muteonText' => t('Mute'),
-  'muteoffText' => t('Un mute'),
-  'quieterText' => t('Quieter'),
-  'louderText' => t('Louder'),
-  'volumeText' => t('Volume'),
+    'muteonText' => t('Mute'),
+    'muteoffText' => t('Un mute'),
+    'quieterText' => t('Quieter'),
+    'louderText' => t('Louder'),
+    'volumeText' => t('Volume'),
 
-  'showscriptText' => t('Show script'),
-  'hidescriptText' => t('Hide script'),
-  'tracksText' => t('Captions'), //"Subtitles" [en-GB]; "Captions" [en-US]
+    'showscriptText' => t('Show script'),
+    'hidescriptText' => t('Hide script'),
+    //'tracksText' => t('Captions'), //"Subtitles" [en-GB]; "Captions" [en-US]
 
-  'progressText' => t('Seek bar'),
-  '_timeText' => t('Time'), //.mejs-time-float-current
-  'currentText' => t('Current time'),
-  'durationText' => t('Total time'),
+    'progressText' => t('Seek bar'),
+    '_timeText' => t('Time'), //.mejs-time-float-current
+    'currentText' => t('Current time'),
+    'durationText' => t('Total time'),
 
-  'qualityText' => t('Quality selection/ High definition'),
-  'popoutText' => t('New window: %s').t('pop out player'),
-  'fullscreenText' => t('Full screen'),
-  'optionsText' => t('More options…'),
-);
+    'qualityText' => t('Quality selection/ High definition'),
+    'popoutText' => t('New window: %s').t('pop out player'),
+    'fullscreenText' => t('Full screen'),
+    'optionsText' => t('More options…'),
+  );
 
 /*if ('my' == $params->lang) {
   foreach ($oup_mep_texts as $key => $val) {
@@ -45,6 +51,7 @@ $oup_mep_texts = array(
   }
 }*/
 
-$texts_json = json_encode($oup_mep_texts);
+  $texts_json = json_encode($oup_mep_texts);
 
-echo trim($texts_json,  '{}') .',';
+  echo trim($texts_json, '{}') .',';
+endif;
