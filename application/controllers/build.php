8758d6e27d79d3e847f9b315bf0d3427b193ea25
Nick Freear <nfreear@yahoo.co.uk>
2012-04-20 12:11:50 +0100

    Re-work of the Build controller - to generate a Closure script including Ender-jeesh.

--- application/controllers/build.php
+++ application/controllers/build.php
@@ -15,38 +15,80 @@
 
 class Build extends MY_Controller {
 
-  public function theme($theme_name = 'oup-light', $optimizations = 'simple', $output = 'ouplayer.min') {	
-    $theme_name = strtolower(str_replace('-', '_', $theme_name));
-    $optimizations = strtoupper($optimizations);
-
-
-    $this->load->theme($theme_name);
-
-    $base_url = base_url() .'application/';
-    $rand = rand(0, 100);
-
-    $closure_template = <<<EOF
-<!doctype html><title>*Closure compiler script | OU Player</title>
-<a href="http://closure-compiler.appspot.com/home">closure-compiler.appspot.com/home</a>
+  protected $_closure_template = <<<EOF
+<h2>__OUTPUT__</h2>
 <pre>
 // ==ClosureCompiler==
-// @output_file_name $output.js
-// @compilation_level {$optimizations}_OPTIMIZATIONS
+// @output_file_name __OUTPUT__
+// @compilation_level __LEVEL__
 //__URLS__// ==/ClosureCompiler==
+
 </pre>
 
 EOF;
 
+
+  public function theme($theme_name = 'oup-light', $optimizations = 'simple', $output = 'ouplayer.min') {	
+    $theme_name = strtolower(str_replace('-', '_', $theme_name));
+    #$optimizations = strtoupper($optimizations);
+
+
+    $this->load->theme($theme_name);
+
+?>
+<!doctype html><title>*Closure compiler script | OU Player</title>
+<a href="http://closure-compiler.appspot.com/home">closure-compiler.appspot.com/home</a>
+<?php
+
+    // Build script for minified Javascripts.
+    echo $this->_closure($this->theme->javascripts, $this->theme->js_min, $optimizations);
+
+    // Build script for minified Javascripts + Ender.
+    $scripts_ender = array_merge(
+      array(
+        //OUP_JS_CDN_ENDER_MIN,
+        'engines/mediaelement/src/js/jeesh.js',
+        'engines/mediaelement/src/js/jeesh-extras.js'
+      ),
+      $this->theme->javascripts
+    );
+    echo $this->_closure($scripts_ender, str_replace('.min','-ender.min', $this->theme->js_min), $optimizations);
+
+?>
+<a href="http://ganquan.info/yui/">ganquan.info/yui</a> | <a href="http://refresh-sf.com/yui/">refresh-sf.com/yui</a>
+<?php
+
+    // Build script for stylesheet.
+    echo $this->_closure($this->theme->styles, $this->theme->css_min, $optimizations);
+  }
+
+  /** Return a Closure build script for a given file array.
+  */
+  protected function _closure($file_array, $output, $comp_level = 'simple') {
+
+    $base_url = base_url().'application/';
+    $rand = rand(0, 100);
+
+    $levels = array(
+      0 => 'WHITESPACE_ONLY',
+      'simple' => 'SIMPLE_OPTIMIZATIONS',
+      'advanced' => 'ADVANCED_OPTIMIZATIONS',
+    );
+    $comp_level = $levels[$comp_level];
+    $output = basename($output);
+
     $url_list = '';
-    foreach ($this->theme->javascripts as $script) {
+    foreach ($file_array as $script) {
       $url_list .= "// @code_url $base_url$script?r=$rand".PHP_EOL;
     }
 
-    echo str_replace('//__URLS__', $url_list, $closure_template);
+    $closure = str_replace(
+      array('//__URLS__', '__OUTPUT__', '__LEVEL__'),
+      array($url_list, $output, $comp_level),
+      $this->_closure_template);
 
-    $this->_css($output);
+    return $closure;
   }
-
 
   protected function _css($output) {
     $base_url = base_url() .'application/';
