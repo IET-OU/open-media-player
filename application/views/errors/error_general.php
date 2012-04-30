1e6e39e315eae76dc1c53608f90363251376af58
Nick Freear <nfreear@yahoo.co.uk>
2012-04-30 16:20:23 +0100

    Bug #1328, Modifying OU-themed error page, to work in small embeds etc.

--- application/views/errors/error_general.php
+++ application/views/errors/error_general.php
@@ -1,116 +1,99 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php
+/**
+ * OU-themed version of 'application/errors/error_general.php'
+ * Uses Ender/jeesh, with a local fallback.
+ *
+ * Copyright 2012 The Open University.
+ */
+if(!function_exists('t')) {
+  function t($s) { return $s; }
+}
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>Podcasts - Learn at Any Time - The Open University</title>
+$base_url = 'http://podcast.open.ac.uk';
+$target = ' target="_blank" title="'.t('Opens in new window').'"';
+
+?>
+<!doctype html><html lang="en"><meta charset=utf-8 />
+  <title><?php echo $heading ?> - The Open University</title>
+  <?php /*
   <meta name="desciption" content="Learn at any time with The Open University audio and video podcasts">
-  <meta name="robots" content="index,nofollow">
-  <meta name="generator" content="The OU Podcasting System by KMi" />
+  <meta name="robots" content="noindex,nofollow">
   <link href="favicon.ico" rel="icon" />
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-  <link href="styles.css" type="text/css" rel="stylesheet" />
-  <link rel="stylesheet" type="text/css" href="/includes/headers-footers/ou-header.css" />
-  <script type="text/javascript" src="/includes/headers-footers/ou-header.js"></script>
-  <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-392384-2']);
-    _gaq.push(['_trackPageview']);
-  
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  </script>
-</head>
+  <link href="styles.css" rel="stylesheet" />*/ ?>
+  <link rel="stylesheet" href="<?php echo $base_url ?>/includes/headers-footers/ou-header.css" />
+  <?php /*<script src="<?php echo $base_url ?>/includes/headers-footers/ou-header.js"></script>*/ ?>
+
+<style>
+.size-small .ou-role-nav, .size-small .ou-ia-nav, .size-small #ou-org-footer, .size-small .searchrow{ display:none;}
+.my-content{ font:1em sans-serif; color:#222; }
+body.size-small{ margin:0 3px; padding:0; font-size:.9em; }
+.size-small .my-content{ float:right; position:relative; top:-85px; }
+.size-small #ou-org-header{ width:86px; position:relative; top:-5px; }
+h2{ margin:1px 0; font-size:1.2em; }
+.searchrow{ float:right; font-size:.8em; }
+.searchrow a{ text-align:right; display:block; margin-top:3px; }
+.my-content{ margin:16px 6px; padding:7px; background:#f4f4f4; border:1px solid #a88; border-radius:4px; }
+.size-small .my-content{ margin:1px; padding:2px 6px; }
+</style>
 
 <body class="ou-ia-community">
-<table class='overalltable' cellpadding='0' cellspacing='0'>
-<tr><td class='includedHeader' colspan='2'><script type="text/javascript">if (typeof window.ou_sitestat=='function')ou_sitestat();</script>
-<div id="ou-org-header"> <a class="ou-skip" href="#ou-content">Skip to content</a> <a class="ou-link-ou" href="http://www.open.ac.uk/"><img src="/includes/headers-footers/oulogo-56.jpg" alt="The Open University" /></a>
+<script>if(typeof window.ou_sitestat=='function')ou_sitestat()</script>
+
+<div id="ou-org-header"> <a class="ou-skip" href="#ou-content">Skip to content</a> <a class="ou-link-ou" href="http://www.open.ac.uk/"<?php echo $target ?>><img src="<?php echo $base_url ?>/includes/headers-footers/oulogo-56.jpg" alt="The Open University" /></a>
   <div class="ou-role-nav">
     <ul>
-      <li class="ou-role-accessibility"><a href="http://www.open.ac.uk/accessibility/">Accessibility</a></li>
+      <li class="ou-role-accessibility"><a href="http://www.open.ac.uk/accessibility/"<?php echo $target ?>>Accessibility</a></li>
       <li class="ou-role-signin" id="ou-signin1"><a href="https://msds.open.ac.uk/signon/sams001.aspx" id="ou-signin2">Sign in</a></li>
       <li id="ou-signout" class="ou-role-signout"><a href="https://msds.open.ac.uk/signon/samsoff.aspx" id="ou-signout2">/ Sign out</a></li>
+<?php /*
       <li id="ou-studenthome" class="ou-role-studenthome"><a href="http://www.open.ac.uk/students/" id="ou-studenthome2">StudentHome</a></li>
       <li id="ou-tutorhome" class="ou-role-tutorhome"><a href="http://www.open.ac.uk/tutorhome/">TutorHome</a></li>
       <li id="ou-intranet" class="ou-role-intranet"><a href="http://intranet.open.ac.uk/">IntranetHome</a></li>
       <li id="ou-sponsor" class="ou-role-sponsor"><a href="https://css2.open.ac.uk/employers/sponsorhome/home/HomePage.aspx">SponsorHome</a></li>
-      <li id="ou-contact" class="ou-role-contact"><a href="http://www.open.ac.uk/contact/">Contact</a></li>
-      <li id="ou-search" class="ou-role-search"><a href="http://www.open.ac.uk/search/">Search the OU</a></li>
+      */ ?>
+<li id="ou-contact" class="ou-role-contact"><a href="http://www.open.ac.uk/contact/"<?php echo $target ?>>Contact</a></li>
+      <li id="ou-search" class="ou-role-search"><a href="http://www.open.ac.uk/search/"<?php echo $target ?>>Search the OU</a></li>
     </ul>
   </div>
-  <div class="ou-ia-nav">
+<div class="ou-ia-nav">
     <ul>
-      <li class="ou-ia-home"><a href="http://www.open.ac.uk/">The Open University</a></li>
-      <li class="ou-ia-study"><a href="http://www.open.ac.uk/study/">Study at the OU</a></li>
-      <li class="ou-ia-research"><a href="http://www.open.ac.uk/research/">Research at the OU</a></li>
-      <li class="ou-ia-community"><a href="http://www.open.ac.uk/community/">OU Community</a></li>
-      <li class="ou-ia-about"><a href="http://www.open.ac.uk/about/">About the OU</a></li>
-    </ul>
+      <li class="ou-ia-home"><a href="http://www.open.ac.uk/"<?php echo $target ?>>The Open University</a></li>
+      <li class="ou-ia-study"><a href="http://www.open.ac.uk/study/"<?php echo $target ?>>Study at the OU</a></li><?php /*
+      <li class="ou-ia-research"><a href="http://www.open.ac.uk/research/"<?php echo $target ?>>Research at the OU</a></li>
+     */ ?> <li class="ou-ia-community"><a href="http://www.open.ac.uk/community/"<?php echo $target ?>>OU Community</a></li>
+      <?php /*<li class="ou-ia-about"><a href="http://www.open.ac.uk/about/"<?php echo $target ?>>About the OU</a></li>
+   */ ?> </ul>
   </div>
-</div></td></tr>
-<tr class='searchrow'>
-<td><table style='width: 100%;' border='0' cellpadding='0' cellspacing='0'><tr><td class='adminmenu'>
-<ul class='adminmenu'>
-<li><a href='/'>Home</a></li>
-<li><a href='https://msds.open.ac.uk/signon/samsoff.aspx?URL=http://podcast.open.ac.uk/signout.php' title='Sign out from SAMS'>Sign out</a></li>
-</ul></td>
-<td class='adminmenu'>
-<ul class='adminmenu'>
-<li><a href='/podcast_list.php' title='View the list of podcasts you created or can edit'>Your Podcasts</a></li>
-<li><a href='/podcast_form.php' title='Provide metadata for a completely new podcast'>New Podcast</a></li>
-</ul>
-</td>
-<td class='adminmenu'>
-</td>
-</tr></table></td>
-<td><b>Search</b> podcasts<br />
-<form action="/search.php" method="post">
-<input type="text" name="searchFor" maxlength="100" />
-</form>
-</td></tr>
-<tr><td class='titleblock'>
-<a id='ou-content' name='ou-content'></a>
-<h1>OU Podcasts</h1>
-<br />
-<p>ERROR: that page does not exist.</p></td>
-<td class='menublock'>
-<div id='cmenu'><h4 class='menuheader'><a href='/oulearn'>OU Learn</a></h4>
-<div class='arrowlistmenu'>
-<h3 class='menuheader'><a href='/oulearn/arts-and-humanities'>Arts and Humanities</a></h3>
-<h3 class='menuheader'><a href='/oulearn/business-and-management'>Business and Management</a></h3>
-<h3 class='menuheader'><a href='/oulearn/childhood-and-youth'>Childhood and Youth</a></h3>
-<h3 class='menuheader'><a href='/oulearn/computing-and-ict'>Computing and ICT</a></h3>
-<h3 class='menuheader'><a href='/oulearn/education'>Education</a></h3>
-<h3 class='menuheader'><a href='/oulearn/engineering-and-technology'>Engineering and Technology</a></h3>
-<h3 class='largemenuheader'><a href='/oulearn/environment-development-and-international-studies'>Environment, Development and International Studies</a></h3>
-<h3 class='menuheader'><a href='/oulearn/health-and-social-care'>Health and Social Care</a></h3>
-<h3 class='menuheader'><a href='/oulearn/languages'>Languages</a></h3>
-<h3 class='menuheader'><a href='/oulearn/law'>Law</a></h3>
-<h3 class='menuheader'><a href='/oulearn/mathematics-and-statistics'>Mathematics and Statistics</a></h3>
-<h3 class='menuheader'><a href='/oulearn/psychology'>Psychology</a></h3>
-<h3 class='menuheader'><a href='/oulearn/science'>Science</a></h3>
-<h3 class='menuheader'><a href='/oulearn/social-sciences'>Social Sciences</a></h3>
 </div>
-<h4 class='menuheader'><a href='/ouresearch'>OU Research</a></h4>
-<h4 class='menuheader'><a href='/oulife'>OU Life</a></h4>
-</div>
-</td></tr>
-</tr>
-<tr><td class='includedHeader' colspan='2'><div id="ou-org-footer">
 
+<div class="my-content">
+
+<form action="http://podcast.open.ac.uk/search.php" method="post"<?php echo $target ?> class="searchrow">
+<label for="search">Search</b> podcasts </label>
+<input id="search" type="search" name="searchFor" maxlength="100" /> <input type="submit" />
+<a href="http://podcast.open.ac.uk/"<?php echo $target ?>>OU Podcasts</a>
+</form>
+
+<a id='ou-content' name='ou-content'></a>
+
+<h2><?php echo $heading ?></h2>
+
+<div><?php echo $message ?></div>
+
+</div>
+
+
+<div id="ou-org-footer">
 <div class="ou-grid ou-mobile-footer" id="ou-mobile-jlinks"></div>
 
 <div class="ou-grid ou-footer-links">
   <div class="ou-c1of4">
     <ul>
-      <li class="ou-title"><a href="http://www.open.ac.uk">The Open University</a></li>
-      <li class="ou-copyright">&#169; Copyright <span id="sbyear"></span>. All rights reserved</li>
-      <li class="ou-phone">+44 (0) 845 300 60 90</li>
-      <li class="ou-email"><a href="http://www.open.ac.uk/email/">Email us</a></li>
+      <li class="ou-title"><a href="http://www.open.ac.uk"<?php echo $target ?>>The Open University</a></li>
+      <li class="ou-copyright">&#169; Copyright <span id="sbyear">2012</span>.<?php /*All rights reserved*/ ?></li>
+      <li class="ou-phone"><!--+44 (0) 8-->0845 300 60 90</li>
+      <li class="ou-email"><a href="http://www.open.ac.uk/email/"<?php echo $target ?>>Email us</a></li>
       <li class="ou-study"><a href="http://www.open.ac.uk/study/">Study at the OU</a></li>
       <li class="ou-research"><a href="http://www.open.ac.uk/research/">Research</a></li>
       <li class="ou-community"><a href="http://www.open.ac.uk/community/">Community</a></li>
@@ -120,18 +103,18 @@
   </div>
   <div class="ou-c2of4">
     <ul>
-    <li class="ou-contact"><a href="http://www.open.ac.uk/contact/">Contact</a></li>
-      <li class="ou-search"><a href="http://www.open.ac.uk/search/">Search</a></li>
+    <?php /*<li class="ou-contact"><a href="http://www.open.ac.uk/contact/">Contact</a></li>
+      <li class="ou-search"><a href="http://www.open.ac.uk/search/">Search</a></li>*/ ?>
       <li class="ou-privacy"><a href="http://www.open.ac.uk/privacy/">Website privacy</a></li>
       <li class="ou-copyright"><a href="http://www.open.ac.uk/copyright/">Copyright</a></li>
-      <li class="ou-conditions"><a href="http://www.open.ac.uk/conditions/">Conditions of use</a></li>
+      <?php /*<li class="ou-conditions"><a href="http://www.open.ac.uk/conditions/">Conditions of use</a></li>*/ ?>
       <li class="ou-cymraeg"><a href="http://www.open.ac.uk/cymraeg/">Cymraeg</a></li>
       <li class="ou-mobile-enquiries">0845 300 60 90</li>
     </ul>
   </div>
   <div class="ou-c3of4">
     <ul>
-      <li class="ou-ia-study"><a href="http://www3.open.ac.uk/study/undergraduate/">Undergraduate</a></li>
+      <?php /*<li class="ou-ia-study"><a href="http://www3.open.ac.uk/study/undergraduate/">Undergraduate</a></li>
       <li class="ou-ia-study"><a href="http://www3.open.ac.uk/study/postgraduate/">Postgraduate</a></li>
       <li class="ou-ia-study ou-ia-research"><a href="http://www.open.ac.uk/research-degrees">Research degrees</a></li>
       <li class="ou-ia-home ou-ia-study"><a href="http://www.open.ac.uk/employers/">Employers</a></li>
@@ -141,7 +124,7 @@
       <li class="ou-ia-about ou-ia-home"><a href="http://www.open.ac.uk/news/">Press Room</a></li>
       <li class="ou-ia-home"><a href="http://www.open.ac.uk/alumni/" class="ou-home">Alumni</a></li>
       <li class="ou-ia-home ou-ia-research ou-ia-about"><a href="http://www.open.ac.uk/jobs/">Jobs</a></li>
-      <li class="ou-ia-home"><a href="http://www.open.ac.uk/fundraising/">Donate</a></li>
+      <li class="ou-ia-home"><a href="http://www.open.ac.uk/fundraising/">Donate</a></li>*/ ?>
       <li class="ou-ia-community"><a href="http://www.open.ac.uk/platform/">Platform</a></li>
       <li class="ou-ia-community"><a href="http://www.open.ac.uk/openlearn/">OpenLearn</a></li>
       <li class="ou-ia-community"><a href="http://www.open.ac.uk/facebook/">Facebook</a></li>
@@ -157,7 +140,38 @@
     
 </div>
 
-<script type="text/javascript">if (typeof window.ou_init=='function')ou_init();</script></td></tr>
-</table>
+<script>if (typeof window.ou_init=='function')ou_init()</script>
+
+
+<?php /*<script src="<?php echo OUP_JS_CDN_JQUERY_MIN ?>"></script>*/ ?>
+<script src="<?php echo OUP_JS_CDN_ENDER_MIN ?>"></script>
+<script>
+if(typeof $=='undefined'){
+  document.write(unescape("%3Cscript src='<?php echo base_url() ?>'engines/mediaelement/src/js/jeesh.js' %3E%3C/script%3E"));
+  CDN_fallback = true;
+}
+</script>
+<script>
+$.domReady(function(){
+  // Set a flag for narrow/ standard/ wide players (POPUP = wide).
+  var body = $('body'),
+	small = 'size-small',
+	medium= 'size-medium',
+	large = 'size-large';
+  function oup_check_size(){
+	if (body.width() <= 450) {
+		body.addClass(small).removeClass(medium).removeClass(large);
+	} else if (body.width() >= 720) {
+		body.removeClass(small).removeClass(medium).addClass(large);
+	} else {
+		body.removeClass(small).addClass(medium).removeClass(large);
+	}
+	console.log('> check_size, width px: '+body.width());
+	//$.log('>> check_size, width px: '+body.width());
+  };
+  $(window).resize(oup_check_size);
+  oup_check_size();
+});
+</script>
 </body>
 </html>
