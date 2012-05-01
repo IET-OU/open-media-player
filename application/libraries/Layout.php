64b51367703b23797e5349b6f228af7c089b2366
Nick Freear <nfreear@yahoo.co.uk>
2012-05-01 20:18:21 +0100

    Bug #1324, Adding Layout library to facilitate user-test pages..
    * From http://codeigniter.com/wiki/layout_library
    * 01:04, 15 Jun 2009   -   adaykin
    * 08:35, 25 Nov 2006   -   CI Toby

---
+++ application/libraries/Layout.php
@@ -0,0 +1,37 @@
+<?php  
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Layout
+{
+    
+    var $obj;
+    var $layout;
+    
+    function Layout($layout = "layout_main")
+    {
+        $this->obj =& get_instance();
+        $this->layout = $layout;
+    }
+
+    function setLayout($layout)
+    {
+      $this->layout = $layout;
+    }
+    
+    function view($view, $data=null, $return=false)
+    {
+        $loadedData = array();
+        $loadedData['content_for_layout'] = $this->obj->load->view($view,$data,true);
+        
+        if($return)
+        {
+            $output = $this->obj->load->view($this->layout, $loadedData, true);
+            return $output;
+        }
+        else
+        {
+            $this->obj->load->view($this->layout, $loadedData, false);
+        }
+    }
+}
+?>
