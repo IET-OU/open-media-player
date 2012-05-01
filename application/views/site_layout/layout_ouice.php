c36b28b481dcaac5e586d1131b166d7cee24db8f
Nick Freear <nfreear@yahoo.co.uk>
2012-05-02 00:02:26 +0100

    Bug #1324, Fixing paths in OUICE 'Study at OU' site layout/template

--- application/views/site_layout/layout_ouice.php
+++ application/views/site_layout/layout_ouice.php
@@ -1,65 +1,79 @@
-<!DOCTYPE HTML>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB" xml:lang="en-GB">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Study Explained | Open University</title>
-    <!-- link to ou webstandards OUICE -->
-<link href="/includes/header.css" rel="stylesheet" type="text/css" media="all" />
-<link href="/includes/ouice/screen.css" rel="stylesheet" type="text/css" media="screen, projection" />
-<link href="/includes/ouice/print.css" rel="stylesheet" type="text/css" media="print" />
-<link href="/includes/headers-footers/header-public-centre-gradient.css" rel="stylesheet" type="text/css" media="all" />
+<?php
+/**
+ * OUICE 3 site template/layout: "Study at the OU"  (Drupal)
+ *
+ * Based on: http://www8.open.ac.uk/study/explained/#org
+ *
+ * @copyright Copyright 2012 The Open University.
+ */
+
+$site_url = $resource_url = 'http://www8.open.ac.uk';
+
+
+?><!doctype html><html lang="en-GB"><meta charset="utf-8" /><title>*Study Explained | Open University</title>
+
+  <meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=0.8" />
+  <meta name="ROBOTS" content="noindex,nofollow" />
+
+  <!-- link to ou webstandards OUICE -->
+<link href="<?php echo $resource_url ?>/includes/header.css" rel="stylesheet" media="all" />
+<link href="<?php echo $resource_url ?>/includes/ouice/screen.css" rel="stylesheet" media="screen, projection" />
+<link href="<?php echo $resource_url ?>/includes/ouice/print.css" rel="stylesheet" media="print" />
+<link href="<?php echo $resource_url ?>/includes/headers-footers/header-public-centre-gradient.css" rel="stylesheet" media="all" />
 <!--[if lt IE 9]>
-  <link rel="stylesheet" type="text/css" href="/includes/ouice/ie.css" />
+  <link rel="stylesheet" href="<?php echo $resource_url ?>/includes/ouice/ie.css" />
 <![endif]-->
 <!--[if lt IE 7]>
-  <link rel="stylesheet" type="text/css" href="/includes/ouice/ie6.css" />
+  <link rel="stylesheet" href="<?php echo $resource_url ?>/includes/ouice/ie6.css" />
 <![endif]-->
-  <link type="text/css" rel="stylesheet" media="all" href="/study/explained/modules/aggregator/aggregator.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/modules/book/book.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/modules/node/node.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/modules/poll/poll.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/modules/system/defaults.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/modules/system/system.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/modules/system/system-menus.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/modules/user/user.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/cck/theme/content-module.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/ctools/css/ctools.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/date/date.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/date/date_popup/themes/datepicker.1.7.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/date/date_popup/themes/jquery.timeentry.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/fckeditor/fckeditor.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/filefield/filefield.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/tagadelic/tagadelic.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/modules/forum/forum.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/misc/farbtastic/farbtastic.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/calendar/calendar.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/cck/modules/fieldgroup/fieldgroup.css?u" />
-<link type="text/css" rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/views/css/views.css?u" />
-<link type="text/css" rel="stylesheet" media="screen" href="/study/explained/sites/all/themes/ou_exstd/styles/style.css?u" />
+<?php /*<link rel="stylesheet" media="all" href="/study/explained/modules/aggregator/aggregator.css?u" />
+  <link rel="stylesheet" media="all" href="/study/explained/modules/book/book.css?u" /> <link rel="stylesheet" media="all" href="/study/explained/modules/node/node.css?u" /> <link rel="stylesheet" media="all" href="/study/explained/modules/poll/poll.css?u" /> <link rel="stylesheet" media="all" href="/study/explained/modules/system/defaults.css?u" /> <link rel="stylesheet" media="all" href="/study/explained/modules/system/system.css?u" /> <link rel="stylesheet" media="all" href="/study/explained/modules/system/system-menus.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/modules/user/user.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/cck/theme/content-module.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/ctools/css/ctools.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/date/date.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/date/date_popup/themes/datepicker.1.7.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/date/date_popup/themes/jquery.timeentry.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/fckeditor/fckeditor.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/filefield/filefield.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/tagadelic/tagadelic.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/modules/forum/forum.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/misc/farbtastic/farbtastic.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/calendar/calendar.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/cck/modules/fieldgroup/fieldgroup.css?u" />
+<link rel="stylesheet" media="all" href="/study/explained/sites/all/modules/contrib/views/css/views.css?u" />
+*/ ?>
+<link rel="stylesheet" media="screen" href="<?php echo $resource_url ?>/study/explained/sites/all/themes/ou_exstd/styles/style.css?u" />
 <!--[if IE 8]>
-  <link href="/study/explained/sites/all/themes/ou_exstd/styles/style_ie8.css" rel="stylesheet" type="text/css" media="screen" />
+  <link href="<?php echo $resource_url ?>/study/explained/sites/all/themes/ou_exstd/styles/style_ie8.css" rel="stylesheet" media="screen" />
 <![endif]-->
 <!--[if IE 7]>
-  <link href="/study/explained/sites/all/themes/ou_exstd/styles/style_ie7.css" rel="stylesheet" type="text/css" media="screen" />
+  <link href="<?php echo $resource_url ?>/study/explained/sites/all/themes/ou_exstd/styles/style_ie7.css" rel="stylesheet" media="screen" />
 <![endif]-->
 <!--[if IE 6]>
-  <link href="/study/explained/sites/all/themes/ou_exstd/styles/style_ie6.css" rel="stylesheet" type="text/css" media="screen" />
-<![endif]-->    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www8.open.ac.uk/study/explained/blogapi/rsd" />
+  <link href="<?php echo $resource_url ?>/study/explained/sites/all/themes/ou_exstd/styles/style_ie6.css" rel="stylesheet" media="screen" />
+<![endif]-->
+
+<?php /*<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www8.open.ac.uk/study/explained/blogapi/rsd" />
 <link rel="next" href="/study/explained/study-explained/building-your-qualification" />
 <link rel="shortcut icon" href="/study/explained/misc/favicon.ico" type="image/x-icon" />
 
-    <meta name="keywords" content="Study explained,,Open University" />    <meta name="robots" content="noodp" />
-</head>
+    <meta name="keywords" content="Study explained,,Open University" />    <meta name="robots" content="noodp" />*/ ?>
+
+
+
+<link rel="stylesheet" href="<?php echo base_url() ?>assets/client/site-embed.css" />
+
+
 <body class="ouice sections i5">
 <div id="org">
-    <script type="text/javascript" src="/includes/headers-footers/header2.js"></script>
-<script type="text/javascript">if (typeof window.ou_sitestat=='function')ou_sitestat();</script>
+    <script src="<?php echo $resource_url ?>/includes/headers-footers/header2.js"></script>
+<script>if (typeof window.ou_sitestat=='function')ou_sitestat();</script>
 <div id="org-header">
 <a id="ou-skip" href="#ou-content">Skip to content</a>
 <div class="ou-hswrap">
 	<div id="hsheader">
-		<a href="http://www.open.ac.uk/" id="hslogo"><img alt="The Open University" src="/includes/headers-footers/oulogo-56.jpg" width="83" height="56" /></a>
+		<a href="http://www.open.ac.uk/" id="hslogo"><img alt="The Open University" src="<?php echo $resource_url ?>/includes/headers-footers/oulogo-56.jpg" width="83" height="56" /></a>
 		<div id="sbhstools">
 			<ul>
 				<li id="ou-accessibility"><a href="http://www.open.ac.uk/accessibility/">Accessibility</a></li>
@@ -97,12 +111,12 @@
             			<!-- Start of site-header -->
                             <!-- End of site-header -->
                         <ul class="sections"><li  class="first menu-8884"><a href="http://www3.open.ac.uk/study/">Study at the OU</a></li>
-<li  class="selected menu-8021 active-trail active"><a href="/study/explained/study-explained" class="selected active">Study explained</a></li>
-<li  class="menu-5778"><a href="/study/explained/is-ou-study-right-for-me">Is OU study right for me?</a></li>
-<li  class="menu-7998"><a href="/study/explained/what-is-distance-learning">What is distance learning?</a></li>
-<li  class="menu-5779"><a href="/study/explained/where-can-i-study">Where can I study?</a></li>
-<li  class="menu-5781"><a href="/study/explained/how-to-apply">How to apply</a></li>
-<li  class="last menu-8969"><a href="/study/explained/fees-2012">Fees 2012</a></li>
+<li  class="selected menu-8021 active-trail active"><a href="<?php echo $site_url ?>/study/explained/study-explained" class="selected active">Study explained</a></li>
+<li  class="menu-5778"><a href="<?php echo $site_url ?>/study/explained/is-ou-study-right-for-me">Is OU study right for me?</a></li>
+<li  class="menu-7998"><a href="<?php echo $site_url ?>/study/explained/what-is-distance-learning">What is distance learning?</a></li>
+<li  class="menu-5779"><a href="<?php echo $site_url ?>/study/explained/where-can-i-study">Where can I study?</a></li>
+<li  class="menu-5781"><a href="<?php echo $site_url ?>/study/explained/how-to-apply">How to apply</a></li>
+<li  class="last menu-8969"><a href="<?php echo $site_url ?>/study/explained/fees-2012">Fees 2012</a></li>
 </ul>                    </div>
         <div id="site-body">
             <div id="page">
@@ -168,17 +182,31 @@
 		</div>
 	</div>
 </div></div>   
-<script type="text/javascript">//<![CDATA[
+<script>//<![CDATA[
 ou_init();
 //]]></script>
 </div>
-<script type="text/javascript" src="/study/explained/misc/jquery.js?u"></script>
-<script type="text/javascript" src="/study/explained/misc/drupal.js?u"></script>
-<script type="text/javascript">
+
+<script src="<?php echo OUP_JS_CDN_JQUERY_MIN ?>"></script>
+
+<?php /*<script src="/study/explained/misc/jquery.js?u"></script>*/ ?>
+<script src="<?php echo $resource_url ?>/study/explained/misc/drupal.js?u"></script>
+<script>
 <!--//--><![CDATA[//><!--
 jQuery.extend(Drupal.settings, { "basePath": "/study/explained/", "jcarousel": { "ajaxPath": "/study/explained/jcarousel/ajax/views" } });
 //--><!]]>
 </script>
-<script type="text/javascript" src="/includes/ouice/scripts.js"></script>
-<script src="/study/explained/sites/all/themes/ou_exstd/scripts/hstheme.js" type="text/javascript"></script></body>
+<script src="<?php echo $resource_url ?>/includes/ouice/scripts.js"></script>
+<script src="<?php echo $resource_url ?>/study/explained/sites/all/themes/ou_exstd/scripts/hstheme.js"></script>
+
+
+
+<script src="<?php echo site_url('scripts/jquery.oembed.js') ?>"></script>
+<script>
+$(document).ready(function() {
+  $("a.embed").oembed(null, {'oupodcast':{'<?php echo OUP_PARAM_THEME ?>':'<?php echo $req->theme ?>'}});<?php /*null, { embedMethod: "replace" });*/ ?>
+});
+</script>
+
+</body>
 </html>
