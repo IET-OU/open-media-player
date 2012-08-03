86671568c6a762490fbcd54556df0bc3b9f5e036
Nick Freear <nfreear@yahoo.co.uk>
2012-08-03 12:40:46 +0100

    Bug #1326, Replace PHP short open tags - VLE demo views..
    * <?= becomes <?php echo ..
    * .. for IT-hosting and code portability.

--- application/views/vle_demo.php
+++ application/views/vle_demo.php
@@ -25,10 +25,10 @@
 sm449.vledev2.open.ac.uk
 <p class="video  vledev">
 <iframe tabindex="0" title="Video player: Introduction to Fairmead" width="640" height="360" frameborder="0"<?php /*scrolling="no"*/ ?> style="overflow:hidden;" src=
-"<?=$embed_url;
+"<?php echo$embed_url;
 ?>?title=Sam's+demo+video&amp;media_url=http%3A//sm449.vledev2.open.ac.uk/moodle/pluginfile.php/5679/mod_resource/content/1/dd301_640x360.mp4&__caption_url=k315-0-video1.srt&width=640&height=360"
 ></iframe>
-<small>A <a href="http://sm449.vledev2.open.ac.uk/moodle/pluginfile.php/5679/mod_resource/content/1/dd301_640x360.mp4">video</a>, <a href="<?=$embed_url;
+<small>A <a href="http://sm449.vledev2.open.ac.uk/moodle/pluginfile.php/5679/mod_resource/content/1/dd301_640x360.mp4">video</a>, <a href="<?php echo$embed_url;
 ?>?title=Introduction+to+Fairmead&amp;media_url=http%3A//sm449.vledev2.open.ac.uk/moodle/pluginfile.php/5679/mod_resource/content/1/dd301_640x360.mp4&amp;caption_url=k315-0-video1.srt&amp;width=512&amp;height=348"
 >player</a>: 512 &times; 348 pixels (318+30, for planned deeper control-bar).</small>
 </p>
@@ -37,17 +37,17 @@ sm449.vledev2.open.ac.uk
 Learn.open.ac.uk
 <p class="video  learn">
 <iframe tabindex="0" title="Video player: Introduction to Fairmead" width="512" height="348" frameborder="0"<?php /*scrolling="no"*/ ?> style="overflow:hidden;" src=
-"<?=$embed_url;
+"<?php echo$embed_url;
 ?>?title=Introduction+to+Fairmead&amp;media_url=http%3A//learn.open.ac.uk/file.php/5195/!via/oucontent/course/100705/k315-0-video1.mp4&__caption_url=k315-0-video1.srt&width=512&height=348"
 ></iframe>
-<small>A <a href="http://learn.open.ac.uk/file.php/5195/!via/oucontent/course/100705/k315-0-video1.mp4">video</a>, <a href="<?=$embed_url;
+<small>A <a href="http://learn.open.ac.uk/file.php/5195/!via/oucontent/course/100705/k315-0-video1.mp4">video</a>, <a href="<?php echo$embed_url;
 ?>?title=Introduction+to+Fairmead&amp;media_url=http%3A//learn.open.ac.uk/file.php/5195/!via/oucontent/course/100705/k315-0-video1.mp4&amp;__caption_url=k315-0-video1.srt&amp;width=512&amp;height=348"
 >player</a>: 512 &times; 348 pixels (318+30, for planned deeper control-bar).</small>
 </p>
 
 <p class="audio  learn">
 <iframe tabindex="0" title="Audio player: Music" width="360" height="60" frameborder="0"<?php /*scrolling="no"*/ ?> style="overflow:hidden;" src=
-"<?=$embed_url;
+"<?php echo$embed_url;
 ?>?title=Music+&copy;Rehab&amp;media_url=http%3A//learn.open.ac.uk/file.php/5195/!via/oucontent/course/137628/20070330_rehab-after.mp3&amp;width=400&amp;height=60"
 ></iframe>
 <small>Audio player: 360 × 60 pixels (fixed size without image_url).</small>
