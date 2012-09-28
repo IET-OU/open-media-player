3aa594dfa3b56ba38d7d47db635f8f835d417de1
Nick Freear <nfreear@yahoo.co.uk>
2012-09-28 15:15:25 +0100

    Bug #1406, IT deployment - fix layout/comments in embed_config.dist.php
    * Re-order so $config[debug], 'data_dir', 'podcast_feed_url_pattern' are 1st
    * Clearer section comments /*-------..

--- application/config/embed_config.dist.php
+++ application/config/embed_config.dist.php
@@ -1,33 +1,64 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+|--------------------------------------------------------------------------
+| OU Media Player / OU-embed main configuration file.
+|--------------------------------------------------------------------------
+*/
 
-/** Configuration: OU player/ embed.
- *
- * @copyright Copyright 2011 The Open University.
- */
 
-// Set a HTTP proxy.
-putenv('http_proxy=wwwcache.open.ac.uk:80');
-$config['http_proxy'] = 'wwwcache.open.ac.uk:80';
-
-// Prevent date/ timezone warnings.
-date_default_timezone_set('Europe/London');
-
-// Debugging.
+/*
+|--------------------------------------------------------------------------
+| Debugging flag (integer, one of OUP_DEBUG_NONE, OUP_DEBUG_MIN, OUP_DEBUG_MAX).
+*/
 $config['debug'] = OUP_DEBUG_NONE;
 
-// Required/Podcast. If TRUE, use the feed model, otherwise, use the database model (requires a config/database.php file).
+
+/*
+|--------------------------------------------------------------------------
+| Web proxy.
+*/
+$config['http_proxy'] = 'wwwcache.open.ac.uk:80';
+
+
+/*
+|--------------------------------------------------------------------------
+| Data directory (required/all).
+*/
+$config['data_dir'] = '/var/www/_ouplayer_data/';
+#$config['data_dir'] = 'C:/Users/NAME/workspace/_ouplayer_data/';
+
+
+/*
+|--------------------------------------------------------------------------
+| Podcast Media Player data-feed (or DB) configuration (required).
+*/
+// If TRUE (default), use the feed model, otherwise, use the database model (requires a config/database.php file).
 $config['podcast_data_use_feed'] = true;
 
 // Required/Podcast. Required for feed access model - the default.
 $config['podcast_feed_url_pattern'] =
     "http://example.org/feeds/__COLLECTION_ID__/rss2-extended.xml";
 
-// (Removed/Podcast. The name of the remote RSS feed file.)
-//$config['podcast_feed_file'] = '';
+
+/*
+|--------------------------------------------------------------------------
+| Podcast Player other configuration.
+*/
+// Placeholder image for private/intranet-only podcasts etc.
+$config['player_restricted_poster_url'] = '/themes/ouplayer_base/pix/locked-video-background-v1.png';
+#$config['player_restricted_poster_url'] = '/themes/ouplayer_base/pix/embossed-video-background-s1-3-v0.png';
+
+// Optional/Podcast. One of NULL, 'iframe' or 'jquery-oembed' (default: NULL)
+$config['player_embed_code'] = NULL;
 
 // (Alternative/Podcast. Required for database access model. No trailing '/')
 //$config['podcast_media_base'] = 'http://example.org/feeds';
 
+
+/*
+|--------------------------------------------------------------------------
+| OUVLE Player configuration.
+*/
 // Optional/VLE: regular expression for media_url, for OUVLE and OpenLearn players.
 // If used, must contain one (ext1|ext2..) group.
 //$config['media_url_regex'] = '/.open.ac.uk\/.*\.(mp4|m4v|flv|mp3)$/';
@@ -35,17 +66,20 @@ $config['podcast_feed_url_pattern'] =
 // Required/VLE/experimental: array of names of session cookies to pass on for caption/subtitle proxy requests (Bug #1334).
 //$config['httplib_proxy_cookies'] = array('cookiename1', 'cookiename2', '..');
 
+
+/*
+|--------------------------------------------------------------------------
+| General OU Player configuration
+*/
 // Either NULL, 'ender' (maybe for OUVLE?) or 'jquery' (maybe for Podcast?)
 // NULL is preferred - it lets Mejs_Default_Theme::prepare_jslib() decide.
 //$config['jslib'] = NULL;
 
-// NEW! A placeholder image for intranet-only podcasts etc.
-#$config['player_restricted_poster_url'] = '/themes/ouplayer_base/pix/embossed-video-background-s1-3-v0.png';
-$config['player_restricted_poster_url'] = '/themes/ouplayer_base/pix/locked-video-background-v1.png';
 
-// Optional/Podcast. One of NULL, 'iframe' or 'jquery-oembed' (default: NULL)
-$config['player_embed_code'] = NULL;
-
+/*
+|--------------------------------------------------------------------------
+| OU-embed configuration.
+*/
 // Debugging/OU-embed: always make requests to upstream servers.
 $config['always_upstream'] = true;
 
@@ -68,16 +102,9 @@ $config['flowplayer_dev'] = false;
 $config['flowplayer_key'] = '#$0...5';
 $config['flowplayer_version'] = '3.2.7';
 
-/*$config['flowplayer_key'] = '#$a...c';
-$config['flowplayer_version'] = '3.1.5';*/
-
 // HTML5 audio/video fallback, see http://wiki.whatwg.org/wiki/Video_type_parameters
 //Was: $config['ouplayer_video_codec'] = 'avc1.42E01E, mp4a.40.2';
 //$config['ouplayer_video_type'] = 'type=\'video/mp4; codecs="avc1.42E01E, mp4a.40.2"\'';
-
-// Required/All. Set the data directory.
-$config['data_dir'] = '/var/www/_ouplayer_data/';
-#$config['data_dir'] = 'C:/Users/ndf42/workspace/_ouplayer_data/';
 
 // Optional. Use if Git is not in the PATH variable.
 //$config['git_path'] = "/usr/local/git/bin/git";  #Mac OS X
@@ -85,12 +112,12 @@ $config['data_dir'] = '/var/www/_ouplayer_data/';
 // Optional. Set the path to the pdftohtml binary (not required on Redhat 6).
 #$config['pdftohtml_path'] = $config['data_dir'].'pdftohtml-0.39/pdftohtml.exe';
 
-// Which embed providers are live on this server?
-$config['providers_local'] = array(
-  'podcast.open.ac.uk'
-);
 
-// OU Player user documentation.
+/*
+|--------------------------------------------------------------------------
+| OU Player user documentation.
+*/
+//
 $player_docs_google = 'https://docs.google.com/document/pub?id=1gcxecBs7n4snPKmQnguBytVZpGdkcjl2GqfGUz-pCOc';
 $config['player_docs'] = array(
   'help' => FALSE,   #"$player_docs_google#id.j2um0zpktyo1",
@@ -98,7 +125,11 @@ $config['player_docs'] = array(
   'embed'=> FALSE,   #"$player_docs_google#id.9xoj8o7ev1d5",
 );
 
-// OU Player themes.
+
+/*
+|--------------------------------------------------------------------------
+| OU Player themes.
+*/
 $config['player_default_theme'] = 'oup-light';
 
 $config['player_themes'] = array(
@@ -116,6 +147,7 @@ $config['player_themes'] = array(
   'ouice-light'=>array('title'=>_('OUICE Light'), 'styles'=>'ouplayer/ouice-light/ouice-light.css', '--menu'=>1),
 );
 
+
 // Captions pre-DB solution - Mental health (S.D.Price@open), http://podcast.open.ac.uk/podcast_items.php?id=1135
 $config['captions'][1135] = array(
   '734418f016' => 'difficult-to-manage-symptoms.xml',
@@ -127,15 +159,10 @@ $config['captions'][1135] = array(
 );
 
 
-/* File: config.php
-
-$config['log_path'] = '/var/www/_ouplayer_data/logs/';
-
-$config['cache_path'] = '/var/www/_ouplayer_data/cache/';
-
+/*
+|--------------------------------------------------------------------------
+| Localization/ internationalization, including 'aliases' (all)
 */
-
-// Localization/ internationalization, including 'aliases'.
 $config['locales'] = array(
     # Keys must be lower-case, using '-'.
     # Order is significant - 'en'+aliases must be last.
@@ -161,5 +188,13 @@ $config['locales'] = array(
     'en-au' => 'en', # Australia
     ... */
 );
+
+
+/* File: config.php
+..
+$config['log_path'] = '/var/www/_ouplayer_data/logs/';
+..
+$config['cache_path'] = '/var/www/_ouplayer_data/cache/';
+*/
 
 #End.
