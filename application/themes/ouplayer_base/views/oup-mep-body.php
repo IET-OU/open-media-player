0e742199983a5f376e6d026cdf869f65a7e80a16
Nick Freear <nfreear@yahoo.co.uk>
2013-05-29 16:39:10 +0100

    Bug #1478/#6952, Volume widget mis-align, Chrome 27
    * Added extra body-class to distinguish Chrome & Safari,
    * It seems that Chrome has fixed a CSS bug which oup-light CSS was accomodating
    * http://iet-it-bugs.open.ac.uk/node/1478
    * http://ltsredmine.open.ac.uk/issues/6952

--- application/themes/ouplayer_base/views/oup-mep-body.php
+++ application/themes/ouplayer_base/views/oup-mep-body.php
@@ -2,7 +2,8 @@
 <!--Body classes - player flags. -->
 <body role="application" id="ouplayer" class="oup mtype-<?php echo $params->media_type ?> mode-<?php echo $mode ?> ctx-<?php echo get_class($params) ?> hide-tscript lang-<?php
   echo $this->lang->lang_code() ?> theme <?php echo $this->theme->name ?> <?php echo $this->theme->rgb ?> bg-<?php echo $this->theme->background ?> ua-<?php echo $this->agent->browser_code()
-  ?> p-<?php echo $this->agent->platform_code() ?> <?php if($this->agent->is_mobile()): ?>is<?php else: ?>not<?php endif; ?>-mobile jslib-<?php echo $this->theme->jslib ?>">
+  ?> br-<?php echo $this->agent->browser() ?> p-<?php echo $this->agent->platform_code() ?> <?php
+  if($this->agent->is_mobile()): ?>is<?php else: ?>not<?php endif; ?>-mobile jslib-<?php echo $this->theme->jslib ?>">
 
 <?php /* Body classes:
 Mediaelement: "oup-mode-video tscript-hide lang-en oup_light ouvle-default-blue ua-webkit p-win jslib-jquery width-large"
