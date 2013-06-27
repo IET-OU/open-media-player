6c342e751384d6896745a8672eff611cd683f665
Nick Freear <nfreear@yahoo.co.uk>
2013-06-27 17:37:03 +0100

    Bug #1477/ #7812, playing on tablet device
    * Fix/ increase video size on mobiles/tablets - `meplayer` view
    * http://iet-it-bugs.open.ac.uk/node/1477

--- application/themes/mejs_default/views/meplayer.php
+++ application/themes/mejs_default/views/meplayer.php
@@ -9,7 +9,7 @@
   $base_url = base_url() . $engine_path;
 
 ?>
-<!doctype html><html lang="en"><meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+<!doctype html><html lang="en" class="mejs-embed"><meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
 <meta charset="utf-8" /><title><?php echo $params->title ?> | <?php //MediaElement ?>OU Player*</title>
 <!--[if lt IE 9]>
 <script src="//html5shim.googlecode.com/svn/trunk/html5-els.js"></script>
@@ -49,9 +49,13 @@ if(typeof jQuery=='undefined'){
 <style>
 /*TODO: fix audio/video player size. */
 
-body.mode-embed{ margin:0; background:transparent; }
+body{ color:#333; font:.95em/1.2 Arial,sans-serif; }
+body.mode-embed, .mejs-embed, .mejs-embed body { margin:0; background:transparent; }
 body.mode-popup{ margin:0; background:#f8f8f8; }
 .mejs-container{ margin:0 auto; }
+.error p{ margin:.6em 0; }
+
+.mejs-embed .mejs-poster img, .mejs-embed video{ display:absolute; top:0; bottom:0; left:0; right:0; x-width:100%; x-height:100%; }
 
 .XX-mode-embed .mejs-container.mejs-audio{ position:fixed; bottom:0; }
 .XX-mode-popup .mejs-container.mejs-audio{ margin-top:20px; }
@@ -82,6 +86,14 @@ body.mode-popup{ margin:0; background:#f8f8f8; }
 
 <body role="application" class="mode-<?php echo $mode ?>">
 
+
+<div id="oup-no-flv" class="error hide" style="display:none">
+  <p class="msg">[<?php echo t(
+    'The video that should appear here can only be played on a computer that has Adobe Flash Player installed. Apologies.'
+  ) ?>]
+</div>
+
+
 <?php
   // Video/ audio height. (iet-it-bugs:1414)
   // (Can't use '100%' - Ender/jeesh chokes on .parent(), .width() or el.style)
@@ -92,6 +104,12 @@ body.mode-popup{ margin:0; background:#f8f8f8; }
   if ('jquery' == $this->theme->jslib) {
     $size_attr = 'width="100%" height="100%"';
     $size_style= '';
+  }
+
+  if ($this->agent->is_mobile() && $params->is_video()) {
+    /*( $size_attr = 'width="480" height="360"'; )*/
+    $size_attr = 'width="320" height="235"';
+    $size_style= ''; #'style="width:100%;height:100%;"';
   }
 ?>
 
@@ -104,6 +122,11 @@ body.mode-popup{ margin:0; background:#f8f8f8; }
  src="<?php echo $params->media_url ?>" type="<?php echo $params->mime_type; #video/mp4 ?>"
  controls="controls" preload="none" <?php if ('video'==$params->media_type): ?>poster="<?php echo $params->poster_url ?>"<?php endif; ?>
 >
+<?php if ($params->caption_url): ?>
+<track kind="subtitles" srclang="en" type="text/vtt" src="<?php
+  // Bug #1334, VLE caption redirect bug [iet-it-bugs:1477] [ltsredmine:6937]
+  echo $_caption_url ?>" />
+<?php endif; ?>
 <?php if ('video'==$params->media_type): ?>
 </video>
 <?php else: ?>
@@ -121,16 +144,38 @@ body.mode-popup{ margin:0; background:#f8f8f8; }
 $(document).ready(function($){
   var player = new mejs.MediaElementPlayer('#player1', {
 
+<?php /*if ($this->theme->is_jquery()): //Includes MSIE 7 and 9 (iet-it-bugs:1474) ?>
+    videoWidth:'100%',
+    videoHeight:'100%',
+<?php endif;*/ ?>
+
+<?php if ($params->poster_url): ?>
+    // url to poster (to fix iOS 3.x)
+    poster:'<?php echo $params->poster_url ?>',
+<?php endif; ?>
+
     // Keyboard accessibility: disable shortcuts!
     enableKeyboard:false,
 
     alwaysShowControls: true,
 
     // path to Flash and Silverlight plugins
-    pluginPath: '<?php echo $base_url ?>build/'
+    pluginPath: '<?php player_res_url($this->theme->plugin_path, $ver = false) ?>'
 
   });
 });
 </script>
 
+<?php if($this->agent->is_mobile()): ?>
+<script>
+if ($("video").get(0).currentSrc.match(/\.flv/)) {
+  $("#oup-no-flv").show();
+}
+</script>
+<?php endif; ?>
+
 </body></html>
+<!--
+<?php echo basename(__FILE__) ?>
+
+-->
