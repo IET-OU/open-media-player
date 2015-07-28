0d174683d512f7878b83afab9909b7cbbe049ed8
Nick Freear <nfreear@yahoo.co.uk>
2015-07-28 10:00:06 +0100

    Bug #46, gh-pages - implement @peterdevinegmd's design part 1 [iet:3813389]
    * Add PNG images + CSS + Google analytics event tracking Javascript

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,9 +4,34 @@
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-var L = window.location;
 
-ga('create', 'UA-24005173-1', 'auto');
-ga('send', 'pageview', '/iet-ou.github.io' + L.pathname + L.search);
+$(function () {
 
-//console.log('pageview', '/iet-ou.github.io' + L.pathname + L.search);
+    var W = window
+      , L = window.location
+      , d = new Date()
+      ;
+
+    ga('create', 'UA-24005173-1', 'auto');
+    ga('send', 'pageview', '/iet-ou.github.io' + L.pathname + L.search);
+
+    //console.log('pageview', '/iet-ou.github.io' + L.pathname + L.search);
+
+    // Event tracking: https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+    $("a").on("click", function (ev) {
+      var url = $(this).attr("href")
+        , text = $(this).text();
+
+      if (url.match(/^https?:/)) {
+        // External URL.
+        ga('send', 'event', 'link', 'click', text +' '+ url);
+
+        console.log("Track extern link click:", text, url);
+      }
+
+      //ev.preventDefault();
+    });
+
+    // Copyright year
+    $("footer .copyright-year").html(d.getYear() + 1900);
+});
