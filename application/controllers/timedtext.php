155e6a040694ca18cf9cbc040c7264e2a7697ae7
Nick Freear <nfreear@yahoo.co.uk>
2019-08-29 16:59:16 +0100

    Bug #93, fix for security vulnerability in `Timedtext` controller
    * Duplicates branch: "master"
    * https://iet.eu.teamwork.com/desk/#/tickets/366419

--- application/controllers/timedtext.php
+++ application/controllers/timedtext.php
@@ -39,6 +39,12 @@ class Timedtext extends \IET_OU\Open_Media_Player\MY_Controller
             $this->_error("Error, 'url' is a required parameter.", 400);
         }
 
+        // Security.
+        if (! preg_match('@^https?:\/\/podcast.open.ac.uk\/@', $ttml_url)) {
+            $this->_error("Error, bad 'url' parameter.", 400);
+            return;
+        }
+
         $p = parse_url($ttml_url);
 
 
